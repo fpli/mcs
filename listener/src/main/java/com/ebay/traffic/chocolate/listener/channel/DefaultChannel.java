62c02f1adc9f104a0f2fc4f0a582f1198f53b97f
Lu huichun <huiclu@ebay.com>
2018-04-17 17:33:00 +0800

    refactor proxy servlet

--- listener/src/main/java/com/ebay/traffic/chocolate/listener/channel/DefaultChannel.java
+++ listener/src/main/java/com/ebay/traffic/chocolate/listener/channel/DefaultChannel.java
@@ -1,30 +1,173 @@
 package com.ebay.traffic.chocolate.listener.channel;
 
+import com.ebay.app.raptor.chocolate.avro.ListenerMessage;
 import com.ebay.app.raptor.chocolate.common.MetricsClient;
+import com.ebay.traffic.chocolate.kafka.KafkaSink;
+import com.ebay.traffic.chocolate.listener.util.ChannelActionEnum;
+import com.ebay.traffic.chocolate.listener.util.ChannelIdEnum;
+import com.ebay.traffic.chocolate.listener.util.ListenerOptions;
+import com.ebay.traffic.chocolate.listener.util.MessageObjectParser;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Request;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.util.Enumeration;
+import java.util.HashMap;
 
 public class DefaultChannel implements Channel {
     private static final Logger logger = Logger.getLogger(DefaultChannel.class);
     private final MetricsClient metrics;
+  private MessageObjectParser parser;
+  private static final String CAMPAIGN_PATTERN = "campid";
+  private static final String SNID_PATTERN = "snid";
 
-    DefaultChannel(MetricsClient metrics) {
-        this.metrics = metrics;
-    }
+  DefaultChannel() {
+    this.metrics = MetricsClient.getInstance();
+    this.parser = MessageObjectParser.getInstance();
+  }
 
     /**
-     * Default channel handler proxies the request and logs a warning
+     * Default channel handler
      */
     @Override
-    public void process(HttpServletRequest servletRequest, HttpServletResponse proxyResponse) {
-        logger.warn("Un-managed channel request: " + servletRequest.getRequestURL());
+    public void process(HttpServletRequest request, HttpServletResponse response) {
+      String kafkaTopic;
+      Producer<Long, ListenerMessage> producer;
+      ChannelActionEnum channelAction;
+      ChannelIdEnum channel;
+      try {
+        if (parser.responseShouldBeFiltered(request, response))
+          return;
+      } catch (MalformedURLException | UnsupportedEncodingException e) {
+        logger.error("Wrong with URL format/encoding", e);
+      }
+
+      long campaignId = getCampaignID(request);
+
+      long startTime = startTimerAndLogData(request);
+
+      String snid = request.getParameter(SNID_PATTERN);
+
+      String[] result = request.getRequestURI().split("/");
+
+      if (result.length == 5) {
+        channel = ChannelIdEnum.parse(result[4]);
+        if (channel == null) {
+          invalidRequestParam(request, "No pattern matched;");
+          return;
+        }
+        channelAction = ChannelActionEnum.parse(channel, result[1]);
+        if (!channel.getLogicalChannel().isValidRoverAction(channelAction)) {
+          invalidRequestParam(request, "Invalid tracking action given a channel;");
+          return;
+        }
+        if (channel.isTestChannel()) {
+          logger.info("Received test URL; URL = " + request.getRequestURI());
+          return;
+        }
+
+        kafkaTopic = ListenerOptions.getInstance().getSinkKafkaConfigs().get(channel.getLogicalChannel().getAvro());
+        producer = KafkaSink.get();
+      } else {
+        logger.warn("Un-managed channel request: " + request.getRequestURL().toString());
         metrics.meter("un-managed");
+        return;
+      }
+
+      // Parse the response
+      ListenerMessage message = parser.parseHeader(request, response,
+          startTime, campaignId, channel.getLogicalChannel().getAvro(), channelAction, snid);
+
+      if (message != null)
+        producer.send(new ProducerRecord<>(kafkaTopic,
+            message.getSnapshotId(), message), KafkaSink.callback);
+      stopTimerAndLogData(startTime, message.toString());
     }
 
-    @Override
-    public long getPartitionKey(final HttpServletRequest servletRequest) {
-        return servletRequest.hashCode();
+  /**
+   * getCampaignId based on query pattern match
+   * Note: parameter is case insensitive
+   *
+   * @param request incoming HttpServletRequest
+   * @return campaignID, default -1L if no pattern match in the query of HttpServletRequest
+   */
+    public long getCampaignID(final HttpServletRequest request) {
+      HashMap<String, String> lowerCaseParams = new HashMap<>();
+      Enumeration params = request.getParameterNames();
+      while (params.hasMoreElements()) {
+        String param = params.nextElement().toString();
+        lowerCaseParams.put(param.toLowerCase(), param);
+      }
+      long campaignId = -1L;
+      String campaign = lowerCaseParams.get(CAMPAIGN_PATTERN);
+
+      if (campaign != null && !request.getParameter(campaign).isEmpty()) {
+        try {
+          campaignId = Long.parseLong(request.getParameter(campaign));
+        } catch (NumberFormatException e) {
+          logger.warn("Invalid campaign: " + campaign);
+        }
+      }
+
+      logger.debug(String.format("PartitionKey: %d", campaignId));
+      return campaignId;
     }
+
+  /**
+   * Stops the timer and logs relevant debugging messages
+   *
+   * @param startTime    the start time, so that latency can be calculated
+   * @param kafkaMessage logged to CAL for debug purposes
+   */
+  private void stopTimerAndLogData(long startTime, String kafkaMessage) {
+    long endTime = System.currentTimeMillis();
+    logger.debug(String.format("EndTime: %d", endTime));
+    metrics.meter("SuccessCount");
+    metrics.mean("AverageLatency", endTime - startTime);
+  }
+
+  /** @return a query message derived from the given string. */
+  private StringBuffer deriveWarningMessage(StringBuffer sb,
+                                            HttpServletRequest servletRequest) {
+    sb.append(" URL=").append(servletRequest.getRequestURL().toString())
+        .append(" queryStr=").append(servletRequest.getQueryString());
+    return sb;
+  }
+
+  /**
+   * Starts the timer and logs some basic info
+   *
+   * @param request Incoming Http request
+   * @return the start time in milliseconds
+   */
+  private long startTimerAndLogData(HttpServletRequest request) {
+    // the main rover process is already finished at this moment
+    // use the timestamp from request as the start time
+    long startTime = System.currentTimeMillis();
+
+    try {
+      startTime = ((Request) request).getTimeStamp();
+    } catch (ClassCastException e) {
+      // ideally only touch this part in unit test
+      logger.warn("Cannot get request start time, use system time instead. ", e);
+    }
+    logger.debug(String.format("StartTime: %d", startTime));
+    metrics.meter("IncomingCount");
+    return startTime;
+  }
+
+  private void invalidRequestParam(HttpServletRequest request, String invalid) {
+    StringBuffer sb = new StringBuffer();
+    sb.append(invalid);
+    sb = deriveWarningMessage(sb, request);
+    logger.warn(sb.toString());
+    logger.warn("Un-managed channel request: " + request.getRequestURL().toString());
+    metrics.meter("un-managed");
+  }
+
 }
--- listener/src/test/java/com/ebay/traffic/chocolate/listener/channel/DefaultChannelTest.java
+++ listener/src/test/java/com/ebay/traffic/chocolate/listener/channel/DefaultChannelTest.java
@@ -1,40 +1,203 @@
 package com.ebay.traffic.chocolate.listener.channel;
 
+import com.ebay.app.raptor.chocolate.avro.ChannelType;
+import com.ebay.app.raptor.chocolate.avro.ListenerMessage;
 import com.ebay.app.raptor.chocolate.common.MetricsClient;
+import com.ebay.traffic.chocolate.kafka.KafkaSink;
+import com.ebay.traffic.chocolate.listener.TestHelper;
+import com.ebay.traffic.chocolate.listener.util.ChannelActionEnum;
+import com.ebay.traffic.chocolate.listener.util.ListenerOptions;
+import com.ebay.traffic.chocolate.listener.util.MessageObjectParser;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.anyObject;
 import static org.mockito.Mockito.*;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ListenerOptions.class, MetricsClient.class, MessageObjectParser.class, KafkaSink.class})
 public class DefaultChannelTest {
-    private DefaultChannel handler;
-    private MetricsClient metrics;
-    private MockHttpServletRequest request;
+  private MockHttpServletRequest mockClientRequest;
+  private MessageObjectParser mockMessageParser;
+  private MetricsClient mockMetrics;
+  private MockHttpServletResponse mockProxyResponse;
+  private Producer mockProducer;
+  private DefaultChannel channel;
 
-    @Before
-    public void setUp() throws Exception{
-        metrics = mock(MetricsClient.class);
-        handler = new DefaultChannel(metrics);
 
-        request = new MockHttpServletRequest();
-        request.setParameter("foo", "bar");
-    }
-    
-    @Test
-    public void testProcessShouldLogUnManagedMetric() throws InterruptedException, ExecutionException, IOException {
-        handler.process(request, mock(HttpServletResponse.class));
-        verify(metrics, times(1)).meter(eq("un-managed"));
-    }
+  @Before
+  public void setUp() {
+   ListenerOptions mockOptions = mock(ListenerOptions.class);
+   PowerMockito.mockStatic(ListenerOptions.class);
+   PowerMockito.when(ListenerOptions.getInstance()).thenReturn(mockOptions);
+   Map<ChannelType, String> channelTopics = new HashMap<>();
+   channelTopics.put(ChannelType.EPN, "epn");
+   channelTopics.put(ChannelType.DISPLAY, "display");
+   PowerMockito.when(mockOptions.getSinkKafkaConfigs()).thenReturn(channelTopics);
 
-    @Test
-    public void testPartitionKey() {
-        Long partitionKey = handler.getPartitionKey(request);
-        assertEquals(Long.valueOf(request.hashCode()), partitionKey);
-    }
+   mockProducer = mock(KafkaProducer.class);
+   PowerMockito.mockStatic(KafkaSink.class);
+   PowerMockito.when(KafkaSink.get()).thenReturn(mockProducer);
+   mockMetrics = mock(MetricsClient.class);
+   PowerMockito.mockStatic(MetricsClient.class);
+   PowerMockito.when(MetricsClient.getInstance()).thenReturn(mockMetrics);
+   mockMessageParser = mock(MessageObjectParser.class);
+   PowerMockito.mockStatic(MessageObjectParser.class);
+   PowerMockito.when(MessageObjectParser.getInstance()).thenReturn(mockMessageParser);
+   mockClientRequest = new MockHttpServletRequest();
+   mockProxyResponse = new MockHttpServletResponse();
+   channel = new DefaultChannel();
+  }
+
+
+  @Test
+  public void campaignIdShouldReturnWhenPresentInQueryString() {
+    long expected = 5337991765L;
+    Map<String, String[]> params = new HashMap<>();
+    params.put("campid", new String[] {"5337991765"});
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    long actual = channel.getCampaignID(mockClientRequest);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void campaignIdShouldRaiseExceptionWhenCampIdInvalid() {
+    long expected = -1L;
+    Map<String, String[]> params = new HashMap<>();
+    params.put("campid", new String[] {"5331AQWAA765"});
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    long actual = channel.getCampaignID(mockClientRequest);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void campaignIdIsCaseInsensitiveAndShouldReturnValueWhenUpperCase() {
+    long expected = 5337991765L;
+    Map<String, String[]> params = new HashMap<>();
+    params.put("CAmpID", new String[] {"5337991765"});
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    long actual = channel.getCampaignID(mockClientRequest);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void campaignIdShouldReturnDefaultIfNoCampaignFound() {
+    long expected = -1L;
+    Map<String, String[]> params = new HashMap<>();
+    params.put("campidxyz", new String[] {"5337991765"});
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    long actual = channel.getCampaignID(mockClientRequest);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void campaignIdShouldReturnDefaultWhenNoCampaignIdFound() {
+    long expected = -1L;
+    Map<String, String[]> params = new HashMap<>();
+    params.put("caMpid", new String[] {""});
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    long actual = channel.getCampaignID(mockClientRequest);
+    assertEquals(expected, actual);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void processShouldSendCorrectMessageToKafkaWithEPNImpressionEvent() throws Exception{
+    final long campaignId = TestHelper.positiveLong();
+    final long snapshotId = TestHelper.positiveLong();
+    Map<String, String[]> params = new HashMap<>();
+    params.put("CaMpid", new String[] {String.valueOf(campaignId)});
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    mockClientRequest.setMethod("get");
+    mockClientRequest.setRequestURI("/roverimp/1/xyz/1");
+    String kafkaMessage = "fake kafka message (EpnChannelTest)";
+
+    DefaultChannel spy = spy(channel);
+    ListenerMessage mockMessage = mock(ListenerMessage.class);
+
+    when(mockMessageParser.responseShouldBeFiltered(mockClientRequest,mockProxyResponse)).thenReturn(false);
+    when(mockMessageParser.parseHeader(eq(mockClientRequest), eq(mockProxyResponse), anyLong(), eq(campaignId),
+        eq(ChannelType.EPN), eq(ChannelActionEnum.IMPRESSION), anyString())).thenReturn(mockMessage);
+    when(mockMessage.getSnapshotId()).thenReturn(snapshotId);
+    when(mockMessage.toString()).thenReturn(kafkaMessage);
+
+    spy.process(mockClientRequest, mockProxyResponse);
+    verify(mockProducer, times(1)).send(new ProducerRecord<>("epn", snapshotId, mockMessage), KafkaSink.callback);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void processShouldSendCorrectMessageToKafkaWithDAPClickEvent() throws Exception{
+    final long campaignId = -1L;
+    final long snapshotId = TestHelper.positiveLong();
+    Map<String, String[]> params = new HashMap<>();
+    params.put("a", new String[] {"b"});
+    mockClientRequest.setParameters(params);
+    mockClientRequest.setMethod("get");
+    mockClientRequest.setRequestURI("/rover/1/xyz/4");
+    String kafkaMessage = "fake kafka message (DAPChannelTest)";
+
+    DefaultChannel spy = spy(channel);
+    ListenerMessage mockMessage = mock(ListenerMessage.class);
+
+    when(mockMessageParser.responseShouldBeFiltered(mockClientRequest,mockProxyResponse)).thenReturn(false);
+    when(mockMessageParser.parseHeader(eq(mockClientRequest), eq(mockProxyResponse), anyLong(), eq(campaignId),
+        eq(ChannelType.DISPLAY), eq(ChannelActionEnum.CLICK), anyString())).thenReturn(mockMessage);
+    when(mockMessage.getSnapshotId()).thenReturn(snapshotId);
+    when(mockMessage.toString()).thenReturn(kafkaMessage);
+
+    spy.process(mockClientRequest, mockProxyResponse);
+    verify(mockProducer, times(1)).send(new ProducerRecord<>("display", snapshotId, mockMessage), KafkaSink.callback);
+  }
+
+  @Test
+  public void processShouldNotSendMessageToKafkaOrJournalIfItCouldNotBeParsed() {
+    ListenerMessage mockMessage = mock(ListenerMessage.class);
+    when(mockMessageParser.parseHeader(eq(mockClientRequest), eq(mockProxyResponse), anyLong(), anyLong(),
+        eq(ChannelType.EPN), eq(ChannelActionEnum.IMPRESSION), eq("")))
+        .thenReturn(mockMessage);
+    channel.process(mockClientRequest, mockProxyResponse);
+    verify(mockProducer, never()).send(new ProducerRecord<>("epn", anyLong(), anyObject()), KafkaSink.callback);
+  }
+
+  @Test
+  public void processShouldNotSendMessageToKafkaOrJournalIfInvalidCampaign() {
+    // with negative number
+    mockClientRequest.setParameter("campid",String.valueOf(-1234L));
+    channel.process(mockClientRequest, mockProxyResponse);
+    verify(mockProducer, never()).send(new ProducerRecord<>("epn", anyLong(), anyObject()), KafkaSink.callback);
+
+    // with string
+    mockClientRequest.setParameter("campid","12345abcde");
+    channel.process(mockClientRequest, mockProxyResponse);
+    verify(mockProducer, never()).send(new ProducerRecord<>("epn", anyLong(), anyObject()), KafkaSink.callback);
+
+    // without campid tag
+    mockClientRequest.setParameter("campxid","12345");
+    channel.process(mockClientRequest, mockProxyResponse);
+    verify(mockProducer, never()).send(new ProducerRecord<>("epn", anyLong(), anyObject()), KafkaSink.callback);
+  }
+
 }
