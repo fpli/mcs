de4faf6c8872734b2dea7ce080662683940aa3a7
jialili1 <jialili1@ebay.com>
2018-05-08 17:37:49 +0800

    change azkaban job in spark-nrt

--- spark-nrt/src/main/azkaban/prod/CappingRule.sh
+++ spark-nrt/src/main/azkaban/prod/CappingRule.sh
@@ -2,8 +2,8 @@
 whoami
 ssh -T -i /usr/azkaban/id_rsa_spark yimeng@lvschocolatepits-1585074.stratus.lvs.ebay.com <<EOSSH
 hostname
-cd /home/spark/chocolate-sparknrt/bin/prod
+cd /home/chocolate/chocolate-sparknrt/bin/prod
 pwd
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
-./cappingRule.sh EPN /apps/tracking-events-workdir /apps/tracking-events 103
+./cappingRule.sh EPN /apps/tracking-events-workdir /apps/tracking-events 150
