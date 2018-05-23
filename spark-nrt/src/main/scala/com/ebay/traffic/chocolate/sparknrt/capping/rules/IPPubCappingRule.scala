1635532843fe38480931cd02e50949d89b30912a
jialili1 <jialili1@ebay.com>
2018-05-23 19:20:24 +0800

    add more comments and small refactor

--- spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/capping/rules/IPPubCappingRule.scala
+++ spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/capping/rules/IPPubCappingRule.scala
@@ -24,16 +24,19 @@ class IPPubCappingRule(params: Parameter, bit: Long, dateFiles: DateFiles, cappi
     $"publisher_id" =!= -1
   }
 
+  //parse IP from request_headers
+  def parseIP(): Column = {
+    split(split($"request_headers", "X-eBay-Client-IP: ")(1), """\|""")(0).alias("IP")
+  }
+
   //counting columns
   def selectCondition(): Array[Column] = {
-    val col0 = split(split($"request_headers", "X-eBay-Client-IP: ")(1), """\|""")(0).alias("IP")
-    Array(col0, cols(1))
+    Array(parseIP(), cols(1))
   }
 
   //add new column if needed
   def withColumnCondition(): Column = {
-    when($"channel_action" === "CLICK", split(split($"request_headers", "X-eBay-Client-IP: ")(1), """\|""")(0))
-        .otherwise("NA")
+    when($"channel_action" === "CLICK", parseIP()).otherwise("NA")
   }
 
   //final join condition
@@ -43,6 +46,7 @@ class IPPubCappingRule(params: Parameter, bit: Long, dateFiles: DateFiles, cappi
 
   override def test(): DataFrame = {
 
+    //Step 1: Prepare counting data
     // filter click only, and publisher_id != -1
     var dfIPPub = dfFilterInJob(filterCondition())
 
@@ -55,18 +59,21 @@ class IPPubCappingRule(params: Parameter, bit: Long, dateFiles: DateFiles, cappi
       val firstRow = head(0)
       val timestamp = dfIPPub.select($"timestamp").first().getLong(0)
 
+      //Step 2: Count by ip and publisher_id in this job, then integrate data to 1 file, and add timestamp to file name
       //count by ip and publisher_id in the job
       dfIPPub = dfCountInJob(dfIPPub, selectCondition())
 
       // reduce the number of counting file to 1, and rename file name to include timestamp
       repartitionAndRename(dfIPPub, timestamp)
 
+      //Step 3: Read a new df for join purpose, and read previous data for counting purpose
       //DataFrame for join
       var df = dfForJoin(cols(0), withColumnCondition())
 
       //read previous data and add to count path
       val ipCountPath = readCountData(timestamp)
 
+      //Step 4: Count all data, including previous data and data in this job, then join the result with the new df
       //count through whole timeWindow and filter those over threshold
       dfIPPub = dfCountAllAndFilter(dfIPPub, ipCountPath)
 
