1b4adde1148441ec7af5192654ed794f211b9ba6
yliu <yliu29@ebay.com>
2018-07-25 13:35:17 +0800

    fix the string case issue for metric

--- filter/src/main/java/com/ebay/app/raptor/chocolate/filter/service/FilterWorker.java
+++ filter/src/main/java/com/ebay/app/raptor/chocolate/filter/service/FilterWorker.java
@@ -144,7 +144,7 @@ public class FilterWorker extends Thread {
             if (offsets.containsKey(tp.partition())) {
               long offset = offsets.get(tp.partition());
               Map<String, Object> additionalFields = new HashMap<>();
-              additionalFields.put("channelType", channelType);
+              additionalFields.put("channelType", channelType.toString());
               additionalFields.put("consumer", tp.partition());
               esMetrics.mean("FilterKafkaConsumerLag", endOffset - offset, additionalFields);
             }
