8eefc1261d53c4118dba755c3f1799c5287f077e
jialili1 <jialili1@ebay.com>
2018-11-06 10:26:40 +0800

    change index date to MST time zone

--- tools/ESDataCleaner/bin/indexCleaner.sh
+++ tools/ESDataCleaner/bin/indexCleaner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XDELETE http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200/%3Cchocolate-log-%7Bnow%2Fd-3d%7D%3E?pretty
+curl -XDELETE http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200/%3Cchocolate-log-%7Bnow%2Fd-3d%7BYYYY.MM.dd%7CMST%7D%7D%3E?pretty
