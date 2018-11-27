b0012ef3e557edd12cf242d74e03fd2a1714d662
Xiang <lixiang1987@icloud.com>
2018-11-27 17:15:38 +0800

    auth in yaml

--- event-listener/src/main/java/com/ebay/app/raptor/chocolate/gen/api/EventsApi.java
+++ event-listener/src/main/java/com/ebay/app/raptor/chocolate/gen/api/EventsApi.java
@@ -10,18 +10,21 @@ import java.util.List;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @Api(description = "The Events API")
-
-@javax.annotation.Generated(value = "com.ebay.swagger.templates.codegen.JavaEtsGenerator", date = "2018-11-20T17:07:46.846+08:00[Asia/Shanghai]")
+@javax.annotation.Generated(value = "com.ebay.swagger.templates.codegen.JavaEtsGenerator", date = "2018-11-27T17:11:51.279+08:00[Asia/Shanghai]")
 public interface EventsApi {
     @POST
     @Path("/events")
     @Consumes({ "application/json" })
-    @ApiOperation(value = "Send one event to marketing tracking", notes = "Send one event to marketing tracking", tags={  })
+    @ApiOperation(value = "Send one event to marketing tracking", notes = "Send one event to marketing tracking", authorizations = {
+        @Authorization(value = "app_scope_auth", scopes = {
+            @AuthorizationScope(scope = "https://api.ebay.com/oauth/scope/@public", description = "This scope would allow public access."),
+@AuthorizationScope(scope = "https://api.ebay.com/oauth/scope/base@public", description = "This scope would allow base public access."),
+@AuthorizationScope(scope = "https://api.ebay.com/oauth/scope/experience@public", description = "This scope would allow experience public access.")        })    }, tags={  })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "OK", response = Void.class),
         @ApiResponse(code = 400, message = "Bad Request", response = Void.class),
         @ApiResponse(code = 500, message = "Internal Server Error", response = Void.class) }
     )
-    
+    @PreAuthorize("hasAuthority('https://api.ebay.com/oauth/scope/@public') and hasAuthority('https://api.ebay.com/oauth/scope/base@public') and hasAuthority('https://api.ebay.com/oauth/scope/experience@public')")
     Response event(Event body);
 }
