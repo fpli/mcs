585e6b8d708cad4ee012508e1418c667caaebb44
jialili1 <jialili1@ebay.com>
2018-12-14 17:20:26 +0800

    add UT and test data for RuleVerifier0

---
+++ spark-nrt/src/test/scala/com/ebay/traffic/chocolate/sparknrt/verifier0/TestRuleVerifier0.scala
@@ -0,0 +1,62 @@
+package com.ebay.traffic.chocolate.sparknrt.verifier0
+
+import com.ebay.traffic.chocolate.spark.BaseFunSuite
+import org.apache.spark.sql.types._
+
+/**
+  * Created by jialili1 on 11/23/18.
+  */
+class TestRuleVerifier0 extends BaseFunSuite {
+
+  val tmpPath = createTempPath()
+  val workDir = tmpPath + "/workDir/"
+  val outputPath = tmpPath + "/outputDir/result"
+
+  val args = Array(
+    "--mode", "local[4]",
+    "--workPath", workDir,
+    "--chocoTodayPath", getTestResourcePath("verifier0/choco_data/part-0001.csv"),
+    "--chocoYesterdayPath", getTestResourcePath("verifier0/choco_data/part-0002.csv"),
+    "--chocoInputFormat", "csv",
+    "--chocoInputDelimiter", "bel",
+    "--epnTodayPath", "",
+    "--epnYesterdayPath", "",
+    "--outputPath", outputPath
+  )
+
+  val schema = StructType(
+    Seq(
+      StructField("snapshot_id", LongType, nullable = true),
+      StructField("timestamp", LongType, nullable = true),
+      StructField("publisher_id", LongType, nullable = true),
+      StructField("campaign_id", LongType, nullable = true),
+      StructField("request_headers", StringType, nullable = true),
+      StructField("uri", StringType, nullable = true),
+      StructField("response_headers", StringType, nullable = true),
+      StructField("rt_rule_flags", LongType, nullable = true),
+      StructField("nrt_rule_flags", LongType, nullable = true),
+      StructField("channel_action", StringType, nullable = true),
+      StructField("channel_type", StringType, nullable = true),
+      StructField("http_method", StringType, nullable = true),
+      StructField("snid", StringType, nullable = true),
+      StructField("is_tracked", BooleanType, nullable = true)
+    )
+  )
+
+  val params = Parameter(args)
+
+  val job = new RuleVerifier0(params)
+
+  override def beforeAll() = {
+    job.setChocoSchema(schema)
+  }
+
+  override def afterAll() = {
+    job.stop()
+  }
+
+  test("Test RuleVerifier0") {
+    job.run()
+  }
+
+}
