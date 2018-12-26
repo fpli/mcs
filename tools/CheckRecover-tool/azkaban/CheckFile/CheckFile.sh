4af0451532df92db0ea55caaad38147a1a3a5f48
Liang Xiong <lxiong1@ebay.com>
2018-12-26 13:59:32 +0800

    add SPARK_EVENTLOG_DIR in the CheckFile.sh

--- tools/CheckRecover-tool/azkaban/CheckFile/CheckFile.sh
+++ tools/CheckRecover-tool/azkaban/CheckFile/CheckFile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 whoami
-ssh -i /usr/azkaban/id_rsa_appdl xiangli4@slcchocolatepits-1242736.stratus.slc.ebay.com <<EOSSH
+ssh -i /usr/azkaban/id_rsa_spark yimeng@lvschocolatepits-1585074.stratus.lvs.ebay.com <<EOSSH
 hostname
-cd /home/hdfs/CheckRecover/
+cd /home/chocolate/tools/CheckRecover-tool/bin/
 pwd
-export HADOOP_USER_NAME=hdfs
+export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
 ./checkFile.sh
--- tools/CheckRecover-tool/bin/checkFile.sh
+++ tools/CheckRecover-tool/bin/checkFile.sh
@@ -3,18 +3,27 @@
 NANOTIMESTAMP=`date +%s%N`
 TIMESTAMP=$(($NANOTIMESTAMP/1000000))
 ESURL=http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200
+export HADOOP_USER_NAME=hdfs
 
+echo $HADOOP_USER_NAME
 echo $TIMESTAMP
 echo $ESURL
 
-/apache/spark/bin/spark-submit \
+. ./chocolate-env.sh
+
+SPARK_EVENTLOG_DIR=hdfs://elvisha/app-logs/chocolate/logs/tools
+
+${SPARK_HOME}/bin/spark-submit \
     --class com.ebay.traffic.chocolate.job.CheckJob \
     --master yarn \
-    --deploy-mode client \
-        CheckRecover-tool-*.jar \
+    --deploy-mode cluster \
+    ${SPARK_JOB_CONF} \
+    --conf spark.eventLog.dir=${SPARK_EVENTLOG_DIR} \
+    --files /home/chocolate/tools/CheckRecover-tool/conf/tasks.xml \
+        /home/chocolate/tools/CheckRecover-tool/lib/CheckRecover-tool-*.jar \
         --appName "CheckJob" \
         --mode "yarn" \
         --countDataDir "" \
-        --ts $IMESTAMP \
-        --taskFile "task.xml" \
+        --ts $TIMESTAMP \
+        --taskFile "tasks.xml" \
         --elasticsearchUrl $ESURL
