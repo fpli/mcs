b90b52a40b2dcb12783bffb16443652c425a19c1
ganghuang <ganghuang@ebay.com>
2019-01-16 16:34:59 +0800

    fix typo

--- spark-nrt/src/main/azkaban/prod/sword_EPN.sh
+++ spark-nrt/src/main/azkaban/prod/sword_EPN.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# this job will move to EPN nrt2batch machine, pending choosing machine
-#whoami
-#ssh -T -i /usr/azkaban/id_rsa_spark yimeng@lvschocolatepits-1585074.stratus.lvs.ebay.com <<EOSSH
-#hostname
-#cd /home/chocolate/chocolate-sparknrt/bin/prod
-#pwd
-#export HADOOP_USER_NAME=chocolate
-#echo $HADOOP_USER_NAME
-#./sword.sh EPN /apps/tracking-events/ /apps/tracking-events-workdir/sword/
+whoami
+ssh -T -i /usr/azkaban/id_rsa_spark yimeng@lvschocolatepits-1585074.stratus.lvs.ebay.com <<EOSSH
+hostname
+cd /home/chocolate/chocolate-sparknrt/bin/prod
+pwd
+export HADOOP_USER_NAME=chocolate
+echo $HADOOP_USER_NAME
+./sword.sh EPN /apps/tracking-events/ /apps/tracking-events-workdir/sword/
