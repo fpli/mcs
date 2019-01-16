702a4d1810a6a471a493bf6aecacb34677bcc60c
Xiang <lixiang1987@icloud.com>
2019-01-16 16:27:57 +0800

    fix listener jetty customize port

--- listener/src/main/java/com/ebay/traffic/chocolate/init/JettyServerConfiguration.java
+++ listener/src/main/java/com/ebay/traffic/chocolate/init/JettyServerConfiguration.java
@@ -5,18 +5,16 @@ import org.eclipse.jetty.server.NetworkTrafficServerConnector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.embedded.JettyWebServerFactoryCustomizer;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.IOException;
 import java.net.URL;
-
 
 @Configuration
 public class JettyServerConfiguration {
@@ -26,29 +24,9 @@ public class JettyServerConfiguration {
   @Autowired
   Environment env;
 
-//  @Bean
-//  public JettyServerCustomizer jettyServerCustomizer() throws IOException {
-//    ListenerOptions.init(env.getProperty(LISTENER_OPTIONS, URL.class).openStream());
-//
-//    ListenerOptions options = ListenerOptions.getInstance();
-//
-//    ListenerInitializer.init(ListenerOptions.getInstance());
-//
-//    return new JettyServerCustomizer() {
-//
-//      @Override
-//      public void customize(Server server) {
-//        final QueuedThreadPool threadPool = server.getBean(QueuedThreadPool.class);
-//        threadPool.setMaxThreads(options.getMaxThreads());
-//        final NetworkTrafficServerConnector connectorHttps = new NetworkTrafficServerConnector(server);
-//        connectorHttps.setPort(options.getInputHttpsPort());
-//        server.addConnector(connectorHttps);
-//      }
-//    };
-//  }
-
   @Bean
-  public JettyServletWebServerFactory jettyEmbeddedServletContainerFactory() throws IOException {
+  public ConfigurableServletWebServerFactory webServerFactory() throws IOException
+  {
     ListenerOptions.init(env.getProperty(LISTENER_OPTIONS, URL.class).openStream());
 
     ListenerOptions options = ListenerOptions.getInstance();
@@ -56,7 +34,6 @@ public class JettyServerConfiguration {
     ListenerInitializer.init(ListenerOptions.getInstance());
 
     JettyServletWebServerFactory factory = new JettyServletWebServerFactory(options.getInputHttpPort());
-
     factory.addServerCustomizers(new JettyServerCustomizer() {
       @Override
       public void customize(Server server) {
@@ -67,7 +44,6 @@ public class JettyServerConfiguration {
         server.addConnector(connectorHttps);
       }
     });
-
     return factory;
   }
 
