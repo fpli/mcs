44aff191e6ee2524fa4a892feacc20c413d72827
Xiang <lixiang1987@icloud.com>
2019-02-26 15:04:06 +0800

    azkaban jobs using datashare

--- spark-nrt/src/main/azkaban/prod/CappingRule_DAP.sh
+++ spark-nrt/src/main/azkaban/prod/CappingRule_DAP.sh
@@ -2,8 +2,6 @@
 whoami
 ssh -T -i /usr/azkaban/id_rsa_spark yimeng@lvschocolatepits-1585074.stratus.lvs.ebay.com <<EOSSH
 hostname
-cd /home/chocolate/chocolate-sparknrt/bin/prod
-pwd
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
-./cappingRule.sh DISPLAY /apps/tracking-events-workdir /apps/tracking-events /apps/tracking-events-archiveDir 150 http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200
+/datashare/mkttracking/jobs/tracking/sparknrt/bin/prod/cappingRule.sh DISPLAY /apps/tracking-events-workdir /apps/tracking-events /apps/tracking-events-archiveDir 150 http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200 false
