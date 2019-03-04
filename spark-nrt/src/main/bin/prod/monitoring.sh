a0a4343e2da03e55f9250369fa982694f3e0f37d
jialili1 <jialili1@ebay.com>
2019-03-04 16:47:07 +0800

    reset monitoring shell

--- spark-nrt/src/main/bin/prod/monitoring.sh
+++ spark-nrt/src/main/bin/prod/monitoring.sh
@@ -23,8 +23,7 @@ EXECUTOR_NUMBER=30
 EXECUTOR_MEMORY=8g
 EXECUTOR_CORES=4
 
-SPARK_EVENTLOG_DIR=hdfs://slickha/app-logs/chocolate/logs/monitoring
-HISTORY_SERVER=http://slcchocolatepits-1242733.stratus.slc.ebay.com:18080/
+SPARK_EVENTLOG_DIR=hdfs://elvisha/app-logs/chocolate/logs/monitoring
 
 JOB_NAME="Monitoring"
 
@@ -46,7 +45,6 @@ ${SPARK_HOME}/bin/spark-submit \
     ${SPARK_JOB_CONF} \
     --conf spark.yarn.executor.memoryOverhead=8192 \
     --conf spark.eventLog.dir=${SPARK_EVENTLOG_DIR} \
-    --conf spark.yarn.historyServer.address=${HISTORY_SERVER} \
     ${bin}/../../lib/chocolate-spark-nrt-*.jar \
       --appName ${JOB_NAME} \
       --mode yarn \
