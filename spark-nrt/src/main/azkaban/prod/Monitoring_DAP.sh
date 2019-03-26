89da4d7cd5fb12f542d1381fb55bb91695c3258f
jialili1 <jialili1@ebay.com>
2019-03-26 17:03:03 +0800

    move monitoring to slc

--- spark-nrt/src/main/azkaban/prod/Monitoring_DAP.sh
+++ spark-nrt/src/main/azkaban/prod/Monitoring_DAP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 whoami
-ssh -T -i /usr/azkaban/id_rsa_spark yimeng@lvschocolatepits-1585074.stratus.lvs.ebay.com <<EOSSH
+ssh -T -i /usr/azkaban/id_rsa_es jialili1@slcchocolatequeue-2168406.stratus.slc.ebay.com <<EOSSH
 hostname
 cd /home/chocolate/chocolate-sparknrt/bin/prod
 pwd
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
-./monitoring.sh DISPLAY /apps/tracking-events-workdir http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200
+./monitoring.sh DISPLAY hdfs://elvisha/apps/tracking-events-workdir http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200
