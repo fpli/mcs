6a9accd560e99907325f9d5ed1476dea0f6ea189
ganghuang <ganghuang@ebay.com>
2019-04-10 16:29:49 +0800

    apollo rno changed to fed

--- spark-nrt/src/main/azkaban/prod/crabRover/scpImkToReno.sh
+++ spark-nrt/src/main/azkaban/prod/crabRover/scpImkToReno.sh
@@ -6,7 +6,7 @@ cd /home/chocolate/chocolate-sparknrt-crab/bin/prod
 pwd
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
-./scpImkToReno.sh /apps/tracking-events/crabTransform/imkOutput hdfs://apollo-rno/apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event /mnt/imkToReno/crab/imkTemp
-./scpImkToReno.sh /apps/tracking-events/imkTransform/imkOutput hdfs://apollo-rno/apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event /mnt/imkToReno/imk/imkTemp
-./scpImkToReno.sh /apps/tracking-events/crabTransform/dtlOutput hdfs://apollo-rno/apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event_dtl /mnt/imkToReno/crab/dtlTemp
-./scpImkToReno.sh /apps/tracking-events/imkTransform/dtlOutput hdfs://apollo-rno/apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event_dtl /mnt/imkToReno/imk/dtlTemp
+./scpImkToReno.sh /apps/tracking-events/crabTransform/imkOutput /apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event /mnt/imkToReno/crab/imkTemp
+./scpImkToReno.sh /apps/tracking-events/imkTransform/imkOutput /apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event /mnt/imkToReno/imk/imkTemp
+./scpImkToReno.sh /apps/tracking-events/crabTransform/dtlOutput /apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event_dtl /mnt/imkToReno/crab/dtlTemp
+./scpImkToReno.sh /apps/tracking-events/imkTransform/dtlOutput /apps/b_marketing_tracking/imk_tracking/imk_rvr_trckng_event_dtl /mnt/imkToReno/imk/dtlTemp
