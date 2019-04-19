11cdf724de6dd2ef42fe8c0e5f09c327f244c90a
Lu huichun <huiclu@ebay.com>
2019-04-19 15:41:31 +0800

    fix bullseye API parameter error

--- spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/epnnrt/BullseyeUtils.scala
+++ spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/epnnrt/BullseyeUtils.scala
@@ -19,7 +19,7 @@ object BullseyeUtils {
   }
 
   @transient lazy val metrics: Metrics = {
-    val url  = properties.getProperty("epnnrt.elasticsearchUrl")
+    val url = properties.getProperty("epnnrt.elasticsearchUrl")
     if (url != null && url.nonEmpty) {
       ESMetrics.init("chocolate-metrics-", url)
       ESMetrics.getInstance()
@@ -30,7 +30,7 @@ object BullseyeUtils {
   def generateToken: JsValue = Http(properties.getProperty("epnnrt.oauthUrl")).method("GET")
     .param("client_id", properties.getProperty("epnnrt.clientId"))
     .param("client_secret", properties.getProperty("epnnrt.clientsecret"))
-    .param("grant_type","client_credentials")
+    .param("grant_type", "client_credentials")
     .param("scope", "https://api.ebay.com/oauth/scope/@public")
     .asString
     .body.parseJson
@@ -39,19 +39,19 @@ object BullseyeUtils {
 
 
   //may be retry here
-  def getData(cguid:String, modelId: String, count: String, bullseyeUrl: String):Option[HttpResponse[String]] = {
+  def getData(cguid: String, modelId: String, count: String, bullseyeUrl: String): Option[HttpResponse[String]] = {
     try {
       logger.debug(s"Bullseye sending, cguid=$cguid")
       val response = Http(bullseyeUrl).method("GET")
-        .header("Authorization",s"Bearer $token")
+        .header("Authorization", s"Bearer $token")
         .param("cguid", cguid)
         .param("modelid", modelId)
         .param("count", count)
         .asString
 
-      if(response.isNotError)
+      if (response.isNotError)
         Some(response)
-       else {
+      else {
         logger.error(s"bullseye response for cguid $cguid with error: $response")
         token = generateToken
         logger.warn(s"get new token: $token")
@@ -59,7 +59,7 @@ object BullseyeUtils {
         None
       }
     } catch {
-      case e : Exception => {
+      case e: Exception => {
         logger.error("error when parse last view item : CGUID:" + cguid + " response: " + e)
         metrics.meter("BullsEyeError", 1)
         None
@@ -67,7 +67,7 @@ object BullseyeUtils {
     }
   }
 
-  def getLastViewItem(cguid: String, timestamp: String, modelId: String, count: String, bullseyeUrl: String): (String, String)= {
+  def getLastViewItem(cguid: String, timestamp: String, modelId: String, count: String, bullseyeUrl: String): (String, String) = {
     val start = System.currentTimeMillis
     val result = getData(cguid, modelId, count, bullseyeUrl)
     metrics.mean("BullsEyeLatency", System.currentTimeMillis - start)
@@ -78,15 +78,15 @@ object BullseyeUtils {
         case null | "" => ("", "")
         case _ =>
           val result_list = new JsonParser().parse(responseBody).getAsJsonArray
-          if(result_list.size() == 1) {
+          if (result_list.size() == 1) {
             //normally there is one result
             val list = new JsonParser().parse(responseBody).getAsJsonArray.get(0).getAsJsonObject.get("results").
               getAsJsonObject.get("response").getAsJsonObject.get("view_item_list").getAsJsonArray
             if (list.size() > 0) {
               for (i <- 0 until list.size()) {
                 var item_id = list.get(i).getAsJsonObject.get("item_id").toString
-                if(!item_id.equalsIgnoreCase("null") && list.get(i).getAsJsonObject.get("timestamp").toString.toLong <= timestamp.toLong){
-                  item_id = item_id.replace("\"","")
+                if (!item_id.equalsIgnoreCase("null") && list.get(i).getAsJsonObject.get("timestamp").toString.toLong <= timestamp.toLong) {
+                  item_id = item_id.replace("\"", "")
                   val date = new Timestamp(list.get(i).getAsJsonObject.get("timestamp").toString.toLong).toString
                   metrics.meter("SuccessfulGet", 1)
                   return (item_id, date)
@@ -97,7 +97,7 @@ object BullseyeUtils {
             //for multiple response results
             var maxLastViwTime = Long.MinValue
             var itemId = ""
-            for (i <-0 until result_list.size()) {
+            for (i <- 0 until result_list.size()) {
               val list = new JsonParser().parse(responseBody).getAsJsonArray.get(i).getAsJsonObject.get("results").
                 getAsJsonObject.get("response").getAsJsonObject.get("view_item_list").getAsJsonArray
               if (list.size() > 0) {
@@ -105,15 +105,15 @@ object BullseyeUtils {
                   var find = false
                   val item_Id = list.get(i).getAsJsonObject.get("item_id").toString
                   val lastViewTime = list.get(i).getAsJsonObject.get("timestamp").toString.toLong
-                  if(!item_Id.equalsIgnoreCase("null") && lastViewTime <= timestamp.toLong && lastViewTime > maxLastViwTime && !find){
-                    itemId = item_Id.replace("\"","")
+                  if (!item_Id.equalsIgnoreCase("null") && lastViewTime <= timestamp.toLong && lastViewTime > maxLastViwTime && !find) {
+                    itemId = item_Id.replace("\"", "")
                     maxLastViwTime = lastViewTime
                     find = true
                   }
                 }
               }
             }
-            if(maxLastViwTime > 0) {
+            if (maxLastViwTime > 0) {
               metrics.meter("SuccessfulGet", 1)
               return (itemId, new Timestamp(maxLastViwTime).toString)
             }
@@ -130,21 +130,21 @@ object BullseyeUtils {
 
 
   // for unit test
-  def getLastViewItemByResponse(timestamp: String, result: HttpResponse[String]): (String, String)= {
+  def getLastViewItemByResponse(timestamp: String, result: HttpResponse[String]): (String, String) = {
     try {
       val responseBody = Some(result).get.body
       responseBody match {
         case null | "" => ("", "")
         case _ =>
           val result_list = new JsonParser().parse(responseBody).getAsJsonArray
-          if(result_list.size() == 1) {
+          if (result_list.size() == 1) {
             val list = new JsonParser().parse(responseBody).getAsJsonArray.get(0).getAsJsonObject.get("results").
               getAsJsonObject.get("response").getAsJsonObject.get("view_item_list").getAsJsonArray
             if (list.size() > 0) {
               for (i <- 0 until list.size()) {
                 var item_id = list.get(i).getAsJsonObject.get("item_id").toString
-                if(!item_id.equalsIgnoreCase("null") && list.get(i).getAsJsonObject.get("timestamp").toString.toLong <= timestamp.toLong){
-                  item_id = item_id.replace("\"","")
+                if (!item_id.equalsIgnoreCase("null") && list.get(i).getAsJsonObject.get("timestamp").toString.toLong <= timestamp.toLong) {
+                  item_id = item_id.replace("\"", "")
                   val date = new Timestamp(list.get(i).getAsJsonObject.get("timestamp").toString.toLong).toString
                   return (item_id, date)
                 }
@@ -153,7 +153,7 @@ object BullseyeUtils {
           } else {
             var maxLastViwTime = Long.MinValue
             var itemId = ""
-              for (i <-0 until result_list.size()) {
+            for (i <- 0 until result_list.size()) {
               val list = new JsonParser().parse(responseBody).getAsJsonArray.get(i).getAsJsonObject.get("results").
                 getAsJsonObject.get("response").getAsJsonObject.get("view_item_list").getAsJsonArray
               if (list.size() > 0) {
@@ -161,15 +161,15 @@ object BullseyeUtils {
                   var find = false
                   val item_Id = list.get(i).getAsJsonObject.get("item_id").toString
                   val lastViewTime = list.get(i).getAsJsonObject.get("timestamp").toString.toLong
-                  if(!item_Id.equalsIgnoreCase("null") && lastViewTime <= timestamp.toLong && lastViewTime > maxLastViwTime && !find){
-                    itemId = item_Id.replace("\"","")
+                  if (!item_Id.equalsIgnoreCase("null") && lastViewTime <= timestamp.toLong && lastViewTime > maxLastViwTime && !find) {
+                    itemId = item_Id.replace("\"", "")
                     maxLastViwTime = lastViewTime
                     find = true
                   }
                 }
               }
             }
-            if(maxLastViwTime > 0) {
+            if (maxLastViwTime > 0) {
               return (itemId, new Timestamp(maxLastViwTime).toString)
             }
           }
