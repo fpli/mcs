24f80a0536be3d642bf83d44478d7c4356052847
ganghuang <ganghuang@ebay.com>
2019-05-09 18:26:52 +0800

    make azkaban shell more common

--- spark-nrt/src/main/azkaban/prod/imkLongTerm/imkDump.sh
+++ spark-nrt/src/main/azkaban/prod/imkLongTerm/imkDump.sh
@@ -3,28 +3,4 @@ CHANNEL=$1
 WORK_DIR=$2
 OUTPUT_DIR=$3
 
-function start_job(){
-    host=$1
-    ssh -T -i /usr/azkaban/id_rsa_spark stack@${host} <<EOSSH
-    hostname
-    cd /datashare/mkttracking/jobs/tracking/chocolate-sparknrt-imk/bin/prod
-    pwd
-    export HADOOP_USER_NAME=chocolate
-    echo $HADOOP_USER_NAME
-
-    ./imkDump.sh ${CHANNEL} ${WORK_DIR} ${OUTPUT_DIR} http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200
-EOSSH
-}
-
-nc -zv lvschocolatepits-1585074.stratus.lvs.ebay.com 22
-rcode=$?
-if [ ${rcode} -eq 0 ]
-then
-    start_job "lvschocolatepits-1585074.stratus.lvs.ebay.com"
-else
-    echo "lvschocolatepits-1585074.stratus.lvs.ebay.com is DOWN, please check!!!"
-    echo "change to lvschocolatepits-1583703.stratus.lvs.ebay.com"
-
-    start_job "lvschocolatepits-1583703.stratus.lvs.ebay.com"
-fi
-
+../SubmitJob.sh "LVS" "./imkDump.sh ${CHANNEL} ${WORK_DIR} ${OUTPUT_DIR} http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200"
