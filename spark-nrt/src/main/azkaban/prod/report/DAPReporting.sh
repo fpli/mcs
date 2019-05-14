23f95764c8dbc7017f354c6321c888e1346e4877
ganghuang <ganghuang@ebay.com>
2019-05-14 10:44:29 +0800

    use HA for spark submit machine

--- spark-nrt/src/main/azkaban/prod/report/DAPReporting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-whoami
-ssh -T -i /usr/azkaban/id_rsa_spark stack@slcchocolatepits-1154246.stratus.slc.ebay.com <<EOSSH
-hostname
-cd /home/chocolate/chocolate-sparknrt-crab/bin/prod
-pwd
-export HADOOP_USER_NAME=chocolate
-echo $HADOOP_USER_NAME
-./nonEpnReporting.sh hdfs://elvisha/apps/tracking-events-workdir hdfs://elvisha/apps/tracking-events-archiveDir DISPLAY all
