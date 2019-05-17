eca5e07f93da95fc2f043faac3abc1b59bf9c4b1
ganghuang <ganghuang@ebay.com>
2019-05-17 17:21:35 +0800

    add azkaban shell for new scpimkto etl

--- spark-nrt/src/main/azkaban/prod/imkLongTerm/imkDump.sh
+++ spark-nrt/src/main/azkaban/prod/imkLongTerm/imkDump.sh
@@ -3,4 +3,4 @@ CHANNEL=$1
 WORK_DIR=$2
 OUTPUT_DIR=$3
 
-../SubmitJob.sh "LVS" "sparknrt" "./imkDump.sh ${CHANNEL} ${WORK_DIR} ${OUTPUT_DIR} http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200"
+../SubmitJob.sh "LVS" "/datashare/mkttracking/jobs/tracking/chocolate-sparknrt-imk/bin/prod" "./imkDump.sh ${CHANNEL} ${WORK_DIR} ${OUTPUT_DIR} http://chocolateclusteres-app-private-11.stratus.lvs.ebay.com:9200"
