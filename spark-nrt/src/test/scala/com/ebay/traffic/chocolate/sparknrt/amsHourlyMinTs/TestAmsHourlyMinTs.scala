27c4f57409179da13e8c0180b6fa9807dde9a7be
jialili1 <jialili1@ebay.com>
2019-06-25 10:15:54 +0800

    add min ts ut

---
+++ spark-nrt/src/test/scala/com/ebay/traffic/chocolate/sparknrt/amsHourlyMinTs/TestAmsHourlyMinTs.scala
@@ -0,0 +1,67 @@
+package com.ebay.traffic.chocolate.sparknrt.amsHourlyMinTs
+
+import java.text.SimpleDateFormat
+
+import com.ebay.app.raptor.chocolate.avro.{ChannelAction, ChannelType, FilterMessage}
+import com.ebay.traffic.chocolate.common.TestHelper
+import com.ebay.traffic.chocolate.spark.BaseFunSuite
+import com.ebay.traffic.chocolate.sparknrt.meta.{DateFiles, MetaFiles, Metadata, MetadataEnum}
+import org.apache.avro.generic.GenericRecord
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.parquet.avro.AvroParquetWriter
+import org.apache.parquet.hadoop.ParquetWriter
+import org.apache.parquet.hadoop.metadata.CompressionCodecName
+
+class TestAmsHourlyMinTs extends BaseFunSuite{
+  private val tmpPath = createTempPath()
+  private val inputDir = getTestResourcePath("amsSample.csv")
+  private val workDir = tmpPath + "/workDir/"
+  private val outputDir = tmpPath + "/outputDir/"
+  private val channel = "EPN"
+  private val usage = "epnnrt_scp_click"
+  private val metaSuffix = ".epnnrt_reno"
+
+  private val sdf = new SimpleDateFormat("yyyy-MM-dd")
+
+  val args = Array(
+    "--mode", "local[8]",
+    "--workDir", workDir,
+    "--channel", channel,
+    "--usage", usage,
+    "--metaSuffix", metaSuffix,
+    "--outputDir", outputDir
+  )
+
+  @transient lazy val hadoopConf = {
+    new Configuration()
+  }
+
+  lazy val fs = {
+    val fs = FileSystem.get(hadoopConf)
+    sys.addShutdownHook(fs.close())
+    fs
+  }
+
+  val params = Parameter(args)
+  val job = new AmsHourlyMinTsJob(params)
+  val timestamp1 = getTimestamp("2019-06-01")
+  val timestamp2 = timestamp1 + 3600000
+  val timestamp3 = timestamp1 + 7200000
+
+  def getTimestamp(date: String): Long = {
+    sdf.parse(date).getTime
+  }
+
+  test("test AmsHourlyMinTs Job") {
+    val metadata = Metadata(workDir, channel, MetadataEnum.epnnrt_scp_click)
+    val dateFiles = DateFiles("date=2019-06-23", Array(inputDir))
+    var meta = new MetaFiles(Array(dateFiles))
+    val outputMetaDir = workDir + "meta/EPN/output/epnnrt_scp_click/"
+    metadata.writeOutputMeta(meta, outputMetaDir, usage, Array(metaSuffix))
+
+    job.run()
+    job.stop()
+  }
+
+}
