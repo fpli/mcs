905bfb01bfd39b96cd7b50454d482c6451a299e0
jialili1 <jialili1@ebay.com>
2019-06-26 17:04:30 +0800

    create rno date dir

--- spark-nrt/src/main/bin/prod/generateAmsHourlyDoneFile.sh
+++ spark-nrt/src/main/bin/prod/generateAmsHourlyDoneFile.sh
@@ -4,41 +4,52 @@ DONE_FILE=$1
 DONE_FILE_DIR=/apps/b_marketing_tracking/watch/
 
 done_date=${DONE_FILE:22:8}
-done_file_full_name=${DONE_FILE_DIR}${done_date}'/'${DONE_FILE}
+done_file_full_dir=${DONE_FILE_DIR}${done_date}
+done_file_full_name=${done_file_full_dir}'/'${DONE_FILE}
 local_done_date_file="/datashare/mkttracking/data/epn-nrt/local_done_date.txt"
 
 #################################### Generate hourly done file on Apollo Rno ####################################
+
 echo "=============== Start generating hourly done file on rno ==============="
 
 HOST_NAME=`hostname -f`
 kinit -kt /datashare/mkttracking/tools/keytab-tool/keytab/b_marketing_tracking.${HOST_NAME}.keytab  b_marketing_tracking/${HOST_NAME}@PROD.EBAY.COM
 
-/datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -test -e ${done_file}
+## Create hourly done date dir if it doesn't exist
+/datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -test -e ${done_file_full_dir}
+done_dir_exists=$?
+if [ ${done_dir_exists} -ne 0 ]
+then
+    /datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -mkdir ${done_file_full_dir}
+fi
+
+## Touch done file, retry 3 times
+/datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -test -e ${done_file_full_name}
 done_file_exists=$?
 if [ ${done_file_exists} -eq 0 ]
 then
     echo "done file exists: ${done_file}"
-    exit 0
+else
+    retry=1
+    rcode_rno=1
+    until [[ ${retry} -gt 3 ]]
+    do
+        /datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -touchz ${done_file_full_name}
+        rcode_rno=$?
+        if [ ${rcode_rno} -eq 0 ]
+        then
+            echo "Successfully generated done file on Apollo Rno: "${done_file_full_name}
+            break
+        else
+            echo "Faild to generate done file on Apollo Rno: "${done_file_full_name}", retrying ${retry}"
+            retry=`expr ${retry} + 1`
+        fi
+    done
 fi
-
-retry=1
-rcode_rno=1
-until [[ ${retry} -gt 3 ]]
-do
-    /datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -touchz ${done_file_full_name}
-    rcode_rno=$?
-    if [ ${rcode_rno} -eq 0 ]
-    then
-         echo "Successfully generated done file on Apollo Rno: "${done_file_full_name}
-         break
-    else
-         echo "Faild to generate done file on Apollo Rno: "${done_file_full_name}", retrying ${retry}"
-         retry=`expr ${retry} + 1`
-    fi
-done
 
 
 #################################### Generate hourly done file on Hercules ####################################
+
 echo "=============== Start generating hourly done file on hercules ==============="
 
 reno_done_file_full_name='viewfs://apollo-rno'${done_file_full_name}
@@ -62,4 +73,5 @@ done
 
 
 ######################################### Save done time to local file #########################################
+
 echo ${DONE_FILE:22:10} > ${local_done_date_file}
