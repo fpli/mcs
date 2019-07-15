381b50dd37639bb071262d99a026ce4850addfdf
jialili1 <jialili1@ebay.com>
2019-07-15 16:35:52 +0800

    add retry to adding partition

--- spark-nrt/src/main/bin/prod/touchAmsHourlyDone.sh
+++ spark-nrt/src/main/bin/prod/touchAmsHourlyDone.sh
@@ -11,22 +11,43 @@ done_file_full_dir=${DONE_FILE_DIR}${done_date}
 done_file_full_name=${done_file_full_dir}'/'${DONE_FILE}
 
 
-################################### Add partition to Hive on Reno and Hercules ###################################
+################################ Add partition to Hive on Reno and Hercules once a day ################################
 
 if [ "${done_hour}" == "00" ]
 then
-    echo "===================== Add partition to Hive on Reno and Hercules ====================="
-    
-    /datashare/mkttracking/tools/apollo_rno/hive_apollo_rno/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table choco_ams_click;"
-    /datashare/mkttracking/tools/apollo_rno/hive_apollo_rno/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table choco_ams_imprsn;"
-    /datashare/mkttracking/tools/hercules_lvs/hive-hercules/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table im_tracking.choco_ams_click;"
-    /datashare/mkttracking/tools/hercules_lvs/hive-hercules/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table im_tracking.choco_ams_imprsn;"
+    echo "======================== Add partition to Hive on Reno and Hercules ========================"
+
+    command_1="/datashare/mkttracking/tools/apollo_rno/hive_apollo_rno/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table choco_ams_click;""
+    command_2="/datashare/mkttracking/tools/apollo_rno/hive_apollo_rno/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table choco_ams_imprsn;""
+    command_3="/datashare/mkttracking/tools/hercules_lvs/hive-hercules/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table im_tracking.choco_ams_click;""
+    command_4="/datashare/mkttracking/tools/hercules_lvs/hive-hercules/bin/hive -e "set hive.msck.path.validation=ignore; MSCK repair table im_tracking.choco_ams_imprsn;""
+
+    retry_repair=1
+    rcode_repair=1
+    until [[ ${retry_repair} -gt 3 ]]
+    do
+        ${command_1} && ${command_2} && ${command_3} && ${command_4}
+        rcode_repair=$?
+        if [ ${rcode_repair} -eq 0 ]
+        then
+            break
+        else
+            echo "Failed to add today's partition to hive."
+            retry=`expr ${retry_repair} + 1`
+        fi
+    done
+    if [ ${rcode_repair} -ne 0 ]
+    then
+        echo -e "Failed to add today's partition on hive!!!" | mailx -S smtp=mx.vip.lvs.ebay.com:25 -s "[NRT ERROR] Error in adding hive partition!!!" -v DL-eBay-Chocolate-GC@ebay.com
+        exit ${rcode_repair}
+    fi
+
 fi
 
 
-######################################## Generate hourly done file on Hercules ########################################
+######################################### Touch hourly done file on Hercules #########################################
 
-echo "===================== Start generating hourly done file on hercules ====================="
+echo "======================= Start touching hourly done file on hercules ======================="
 
 ## Create hourly done date dir if it doesn't exist
 /datashare/mkttracking/tools/hercules_lvs/hadoop-hercules/bin/hadoop fs -test -e ${done_file_full_dir}
@@ -62,13 +83,13 @@ else
 fi
 if [ ${rcode} -ne 0 ]
     then
-        echo -e "Fail to touch hourly done file: ${done_file_full_name} to Hercules!!!" | mailx -S smtp=mx.vip.lvs.ebay.com:25 -s "NRT Error!!!(Hourly done file to Hercules)" -v DL-eBay-Chocolate-GC@ebay.com
+        echo -e "Failed to touch hourly done file: ${done_file_full_name} on Hercules!!!" | mailx -S smtp=mx.vip.lvs.ebay.com:25 -s "[NRT ERROR] Error in touching hourly done file on Hercules!!!" -v DL-eBay-Chocolate-GC@ebay.com
         exit ${rcode}
 fi
 
 
 ############################################ Save done time to local file ############################################
 
-echo "===================== Save done time to local file ====================="
+echo "=============================== Save done time to local file ==============================="
 
 echo ${DONE_FILE:22:10} > ${LOCAL_DONE_DATE_FILE}
