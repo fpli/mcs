9915f722a524b98e156e7582508b175123dd06ec
Xiang <xiangli4@ebay.com>
2019-08-27 17:54:53 +0800

    parse roi client id from rover url

--- spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/imkDump/ImkDumpRoiJob.scala
+++ spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/imkDump/ImkDumpRoiJob.scala
@@ -38,7 +38,7 @@ class ImkDumpRoiJob(params: Parameter) extends ImkDumpJob(params: Parameter) {
   override def imkDumpCore(df: DataFrame): DataFrame = {
     val imkDf = super.imkDumpCommon(df)
       .drop("dst_client_id")
-      .withColumn("dst_client_id", getClientIdFromRotationIdUdf(col("dst_rotation_id")))
+      .withColumn("dst_client_id", getClientIdFromRoverUrlUdf(col("temp_uri_query")))
       .withColumn("roi_item_id", getItemIdUdf(col("uri"), col("channel_type")))
       .withColumn("transaction_id", getRoiIdsUdf(lit(2), col("temp_uri_query")))
       .withColumn("transaction_type", getParamFromQueryUdf(col("temp_uri_query"), lit("tranType")))
@@ -51,5 +51,6 @@ class ImkDumpRoiJob(params: Parameter) extends ImkDumpJob(params: Parameter) {
   }
 
   val getRoiIdsUdf: UserDefinedFunction = udf((index: Int, uri: String) => tools.getRoiIdFromUri(index, uri))
-  val getClientIdFromRotationIdUdf: UserDefinedFunction = udf((rotationId: String) => tools.getClientIdFromRotationId(rotationId))
+  //TODO: deprecate this when move ROI out of rover
+  val getClientIdFromRoverUrlUdf: UserDefinedFunction = udf((uri: String) => tools.getClientIdFromRoverUrl(uri))
 }
