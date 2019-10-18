d41ca462d27ff13d638d73790a4ba7fcb5a51262
jialili1 <jialili1@ebay.com>
2019-10-18 11:33:34 +0800

    decode sojTags param

--- event-listener/src/main/java/com/ebay/app/raptor/chocolate/eventlistener/CollectionService.java
+++ event-listener/src/main/java/com/ebay/app/raptor/chocolate/eventlistener/CollectionService.java
@@ -38,6 +38,7 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -408,9 +409,8 @@ public class CollectionService {
     // add tags in url param "sojTags"
     addGenericSojTags(requestContext, parameters, referer, type, action);
 
-    // TODO apply for a new page id for email open
     // add tags all channels need
-    addCommonTags(requestContext, null, referer, agentInfo, type, action, 2547208);
+    addCommonTags(requestContext, null, referer, agentInfo, type, action, 3962);
 
     // add channel specific tags, and produce message for EPN and IMK
     boolean processFlag = false;
@@ -653,6 +653,12 @@ public class CollectionService {
 
       if(parameters.containsKey(Constants.SOJ_TAGS)) {
         String sojTags = parameters.get(Constants.SOJ_TAGS).get(0);
+        try {
+          sojTags = URLDecoder.decode(sojTags, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+          logger.warn("Param sojTags is wrongly encoded", e);
+          metrics.meter("ErrorEncodedSojTags", 1, Field.of(CHANNEL_ACTION, action), Field.of(CHANNEL_TYPE, type));
+        }
         if (!StringUtils.isEmpty(sojTags)) {
           StringTokenizer stToken = new StringTokenizer(sojTags, PresentationConstants.COMMA);
           while (stToken.hasMoreTokens()) {
