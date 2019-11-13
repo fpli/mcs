be0ba4124d89048d40d7bc04d15743ec6cf25cec
Liang Xiong <lxiong1@ebay.com>
2019-11-13 10:17:54 +0800

    Check the length of the inteval and date string length before rm

--- tools/HdfsDataClean/bin/dataClean.sh
+++ tools/HdfsDataClean/bin/dataClean.sh
@@ -2,10 +2,25 @@
 
 export HADOOP_USER_NAME=hdfs
 
-# keep the data for 7 days
+# keep the data for 8 days
 DIFF=8
 DATE=`date --date=$DIFF" days ago" +%Y-%m-%d`
-echo $DATE
+echo "DIFF $DIFF"
+echo "DATE $DATE"
+
+if [ ! -n "$DATE" ] || [ ! -n "$DIFF" ]; then
+  echo "The length of DATE or DIFF is zero, exit."
+  exit
+else
+  echo "Both the length of DATE and DIFF are not zero."
+fi
+
+if [ "$DIFF" -lt 8 ]; then
+  echo "But diff is less than 8, exit."
+  exit
+else
+  echo "And diff is greater than or equals 8."
+fi
 
 hdfs dfs -rm -r hdfs://elvisha/apps/tracking-events/EPN/capping/date=$DATE
 hdfs dfs -rm -r hdfs://elvisha/apps/tracking-events/EPN/dedupe/date=$DATE
@@ -24,10 +39,26 @@ hdfs dfs -rm -r hdfs://elvisha/apps/tracking-events/SOCIAL_MEDIA/imkDump/date=$D
 
 hdfs dfs -rm -r hdfs://slickha/apps/tracking-events/crabDedupe/date=$DATE
 
+
 # keep the data for 60 days
 DIFF60=60
 DATE60=`date --date=$DIFF60" days ago" +%Y-%m-%d`
-echo $DATE60
+echo "DIFF60 $DIFF60"
+echo "DATE60 $DATE60"
+
+if [ ! -n "$DATE60" ] || [ ! -n "$DIFF60" ]; then
+  echo "The length of DATE60 or DIFF60 is zero, exit."
+  exit
+else
+  echo "Both the length of DATE60 and DIFF60 are not zero."
+fi
+
+if [ "$DIFF60" -lt 60 ]; then
+  echo "But diff is less than 60, exit."
+  exit
+else
+  echo "And diff is greater than or equals 60."
+fi
 
 hdfs dfs -rm -r hdfs://elvisha/apps/epn-nrt/click/date=$DATE60
 hdfs dfs -rm -r hdfs://elvisha/apps/epn-nrt/impression/date=$DATE60
