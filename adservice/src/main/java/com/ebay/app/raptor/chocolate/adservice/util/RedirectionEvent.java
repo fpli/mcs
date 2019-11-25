f509d64e2544913e290e1122e215b59ff14227a0
jialili1 <jialili1@ebay.com>
2019-11-25 15:19:57 +0800

    add redirection event

---
+++ adservice/src/main/java/com/ebay/app/raptor/chocolate/adservice/util/RedirectionEvent.java
@@ -0,0 +1,75 @@
+package com.ebay.app.raptor.chocolate.adservice.util;
+
+import com.ebay.app.raptor.chocolate.constant.ChannelIdEnum;
+
+/**
+ * Redirection event information
+ *
+ * Created by jialili1 on 11/21/19
+ */
+public class RedirectionEvent {
+  private String redirectUrl;
+  private String redirectSource;
+  private String partnerId;
+  private String channelId;
+  private String channelType;
+  private String actionId;
+
+  public RedirectionEvent(String channelId, String actionId, String partnerId) {
+    this.channelId = channelId;
+    this.channelType = ChannelIdEnum.parse(channelId).getLogicalChannel().getAvro().toString();
+    this.actionId = actionId;
+    this.partnerId = partnerId;
+  }
+
+  public void setRedirectUrl(String redirectUrl) {
+    this.redirectUrl = redirectUrl;
+  }
+
+  public String getRedirectUrl() {
+    return redirectUrl;
+  }
+
+  public void setRedirectSource(String redirectSource) {
+    this.redirectSource = redirectSource;
+  }
+
+  public String getRedirectSource() {
+    return redirectSource;
+  }
+
+  public void setPartnerId(String partnerId) {
+    this.partnerId = partnerId;
+  }
+
+  public String getPartnerId() {
+    return partnerId;
+  }
+
+  public String getPartner() {
+    if ("14".equals(partnerId)) {
+      return "adobe";
+    } else
+      return null;
+  }
+
+  public void setChannelType(String channel) {
+    this.channelType = channel;
+  }
+
+  public String getChannelType() {
+    return channelType;
+  }
+
+  public void setActionId(String action) {
+    this.actionId = action;
+  }
+
+  public String getActionId() {
+    return actionId;
+  }
+
+  public String getChannelId() {
+    return channelId;
+  }
+}
