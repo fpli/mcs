68189d7c9224be0969859ec9cb18e7c09a6dbaae
Liang Xiong <lxiong1@ebay.com>
2020-01-13 19:50:14 +0800

    add xushuang imk hourly count report.

--- tools/HdfsDataClean/azkaban/DataClean/DataClean.sh
+++ tools/HdfsDataClean/azkaban/DataClean/DataClean.sh
@@ -2,7 +2,7 @@
 whoami
 ssh -i /usr/azkaban/id_rsa_spark stack@slcchocolatepits-1242736.stratus.slc.ebay.com <<EOSSH
 hostname
-/datashare/mkttracking/tools/HdfsDataClean/bin
+cd /datashare/mkttracking/tools/HdfsDataClean/bin/
 pwd
 export HADOOP_USER_NAME=hdfs
 echo $HADOOP_USER_NAME
