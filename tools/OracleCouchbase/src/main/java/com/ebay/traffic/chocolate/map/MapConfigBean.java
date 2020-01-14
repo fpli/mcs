e60f18941aaf12621d0dbb93a9bdaca4aa0b6dd9
Liang Xiong <lxiong1@ebay.com>
2020-01-14 13:34:15 +0800

    change the version to 3.6.0-RELEASE

--- tools/OracleCouchbase/src/main/java/com/ebay/traffic/chocolate/map/MapConfigBean.java
+++ tools/OracleCouchbase/src/main/java/com/ebay/traffic/chocolate/map/MapConfigBean.java
@@ -1,7 +1,6 @@
 package com.ebay.traffic.chocolate.map;
 
 import com.ebay.kernel.bean.configuration.*;
-import com.ebay.traffic.chocolate.monitoring.ESMetrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -17,12 +16,10 @@ import javax.annotation.PostConstruct;
 public class MapConfigBean extends BaseConfigBean {
   private static BeanPropertyInfo ORACLE_DRIVER = createBeanPropertyInfo("oracleDriver", "oracle_driver", true);
   private static BeanPropertyInfo ORACLE_URL = createBeanPropertyInfo("oracleUrl", "oracle_url", true);
-  private static BeanPropertyInfo ES_URL = createBeanPropertyInfo("esEndpoint", "es_endpoint", true);
   private static BeanPropertyInfo EPN_COUCHBASE_DATA_SOURCE = createBeanPropertyInfo("epnCbDataSource", "epn_couchbase_data_source", true);
   private static BeanPropertyInfo CP_COUCHBASE_DATA_SOURCE = createBeanPropertyInfo("cpCbDataSource", "cp_couchbase_data_source", true);
 
 
-  private String esEndpoint;
   private String oracleDriver;
   private String oracleUrl;
   /**
@@ -34,27 +31,17 @@ public class MapConfigBean extends BaseConfigBean {
    */
   private String cpCbDataSource;
 
-  private static final String METRICS_INDEX_PREFIX = "mktoracle-metrics-";
   private static final Logger logger = LoggerFactory.getLogger(MapConfigBean.class);
 
   public MapConfigBean() throws ConfigCategoryCreateException {
     BeanConfigCategoryInfo categoryInfo = new BeanConfigCategoryInfoBuilder()
       .setCategoryId("com.ebay.traffic.chocolate.MapConfigBean")
-      .setAlias("trackapi")
-      .setGroup("MktTrackSvc")
+      .setAlias("trackAlert")
+      .setGroup("MktTrackMonitor")
       .setPersistent(true)
       .build();
     // this init method need to be called to bind the ConfigBean instance to a configuration category
     init(categoryInfo, true);
-  }
-
-  @PostConstruct
-  public void setESClient() {
-    ESMetrics.init(METRICS_INDEX_PREFIX, this.getEsEndpoint());
-//        ESMetrics esMetrics = ESMetrics.getInstance();
-    logger.info("EsEndpoint is: " + this.getEsEndpoint());
-//        logger.info("already configured esMetrics: " + esMetrics);
-    logger.info("Initialize ElasticSearch client successfully.");
   }
 
   public String getOracleDriver() {
@@ -73,15 +60,6 @@ public class MapConfigBean extends BaseConfigBean {
   public void setOracleUrl(String oracleUrl) {
     changeProperty(ORACLE_URL, this.oracleUrl, oracleUrl);
     logger.info("oracleUrl has been changed: " + oracleUrl);
-  }
-
-  public String getEsEndpoint() {
-    return esEndpoint;
-  }
-
-  public void setEsEndpoint(String esEndpoint) {
-    changeProperty(ES_URL, this.esEndpoint, esEndpoint);
-    logger.info("ES_URL has been changed: " + esEndpoint);
   }
 
   public String getEpnCbDataSource() {
