a82789e139ceb0683a4a99840121195ac5a6190f
Xiang <xiangli4@ebay.com>
2020-01-31 15:59:48 +0800

    add same site cookie tag

--- adservice/src/main/java/com/ebay/app/raptor/chocolate/adservice/util/AdserviceCookie.java
+++ adservice/src/main/java/com/ebay/app/raptor/chocolate/adservice/util/AdserviceCookie.java
@@ -4,6 +4,7 @@ import com.ebay.traffic.monitoring.ESMetrics;
 import com.ebay.traffic.monitoring.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.Cookie;
@@ -67,9 +68,15 @@ public class AdserviceCookie {
     if(adguid == null) {
       metrics.meter(METRIC_SET_NEW_ADGUID);
       adguid = UUID.randomUUID().toString();
-      Cookie cookie = new Cookie(ADGUID, adguid);
-      cookie.setMaxAge(COOKIE_EXPIRY);
-      response.addCookie(cookie);
+
+      ResponseCookie cookie = ResponseCookie.from(ADGUID, adguid)
+          .maxAge(COOKIE_EXPIRY)
+          .path("/")
+          .sameSite("None")
+          .secure(true)
+          .build();
+
+      response.addHeader("Set-Cookie", cookie.toString());
     }
     return adguid;
   }
