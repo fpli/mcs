6ad6be0f8b210489fa3978c9749214c49fcf5de8
zhiyuawang <zhiyuawang@ebay.com>
2020-04-26 13:36:23 +0800

    handle dap redirect

---
+++ adservice/src/main/java/com/ebay/app/raptor/chocolate/adservice/constant/Headers.java
@@ -0,0 +1,13 @@
+package com.ebay.app.raptor.chocolate.adservice.constant;
+
+public class Headers {
+  public static final String REDIRECT_URL = "redirect_url";
+  public static final String ACCEPT = "Accept";
+  public static final String PRAGMA = "Pragma";
+  public static final String LOCATION = "Location";
+  public static final String CHARSET = "charset=";
+  public static final String CACHE_CONTROL = "Cache-Control";
+  public static final String CONTENT_TYPE_HTML = "text/html";
+  public static final String CONTENT_TYPE = "Content-Type";
+  public static final char SEMICOLON = ';';
+}
