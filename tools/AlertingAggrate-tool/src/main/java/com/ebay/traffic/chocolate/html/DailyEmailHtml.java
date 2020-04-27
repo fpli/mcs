3e828c503fbb0d6220a02fd917fc25bea4607af1
Liang Xiong <lxiong1@ebay.com>
2020-04-27 18:16:40 +0800

    add try catch.

--- tools/AlertingAggrate-tool/src/main/java/com/ebay/traffic/chocolate/html/DailyEmailHtml.java
+++ tools/AlertingAggrate-tool/src/main/java/com/ebay/traffic/chocolate/html/DailyEmailHtml.java
@@ -2,35 +2,74 @@ package com.ebay.traffic.chocolate.html;
 
 import com.ebay.traffic.chocolate.parse.EPNReportUtil;
 import com.ebay.traffic.chocolate.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DailyEmailHtml {
 
+  private static final Logger logger = LoggerFactory.getLogger(HourlyEmailHtml.class);
+
   public static String getESAlertHtml(String runPeriod) {
-    return Table.parseESAlertProjects(ESAlertUtil.getESAlertInfos(runPeriod), null);
+    try {
+      return Table.parseESAlertProjects(ESAlertUtil.getESAlertInfos(runPeriod), null);
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getESAlertHtml";
+    }
   }
 
   public static String getHdfsCompareHtml() {
-    return "Hdfs file number compare\n" + HdfsCompareTable.parseHdfsCompare(HdfsCompareUtil.getHdfsFileNumberCompares());
+    try {
+      return "Hdfs file number compare\n" + HdfsCompareTable.parseHdfsCompare(HdfsCompareUtil.getHdfsFileNumberCompares());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getHdfsCompareHtml";
+    }
   }
 
   public static String getTDRotationCountHtml() {
-    return "Rotation count in TD\n" + TDRotationCountTable.parseTDRotationCountProject(TDRotationCountUtil.getTDRotationInfos());
+    try {
+      return "Rotation count in TD\n" + TDRotationCountTable.parseTDRotationCountProject(TDRotationCountUtil.getTDRotationInfos());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getTDRotationCountHtml";
+    }
   }
 
   public static String getTDIMKCountHtml() {
-    return "All channel count in TD\n" + TDIMKCountTable.parseTDIMKCountProject(TDIMKCountUtil.getTDIMKInfos());
+    try {
+      return "All channel count in TD\n" + TDIMKCountTable.parseTDIMKCountProject(TDIMKCountUtil.getTDIMKInfos());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getTDIMKCountHtml";
+    }
   }
 
   public static String getEPNDailyReportHtml() {
-    return EPNReportUtil.getDailyReport();
+    try {
+      return EPNReportUtil.getDailyReport();
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getEPNDailyReportHtml";
+    }
   }
 
   public static String getBenchMarkHtml() {
-    return "All channel benchmark\n" + BenchMarkTable.parseBenchMarkProject(BenchMarkUtil.getBenchMarkInfos());
+    try {
+      return "All channel benchmark\n" + BenchMarkTable.parseBenchMarkProject(BenchMarkUtil.getBenchMarkInfos());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getBenchMarkHtml";
+    }
   }
 
   public static String getOralceAndCouchbaseCountHtml() {
-    return "Epn campaign mapping count in the oralce and couchbase\n" + OracleAndCouchbaseCountTable.parseOracleAndCouchbaseCountProject(OracleAndCouchbaseCountUtil.getOracleAndCouchbaseCountInfos());
+    try {
+      return "Epn campaign mapping count in the oralce and couchbase\n" + OracleAndCouchbaseCountTable.parseOracleAndCouchbaseCountProject(OracleAndCouchbaseCountUtil.getOracleAndCouchbaseCountInfos());
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return "getOralceAndCouchbaseCountHtml";
+    }
   }
 
 }
