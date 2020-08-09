56c5a7de3f5dd69a6359853f63ed48d5782e25e9
zhiyuawang <zhiyuawang@ebay.com>
2020-08-09 18:45:45 +0800

    sink behavior event to hdfs

--- flink-nrt/src/main/java/com/ebay/traffic/chocolate/flink/nrt/app/BehaviorEventSinkApp.java
+++ flink-nrt/src/main/java/com/ebay/traffic/chocolate/flink/nrt/app/BehaviorEventSinkApp.java
@@ -1,24 +1,23 @@
 package com.ebay.traffic.chocolate.flink.nrt.app;
 
-import com.ebay.app.raptor.chocolate.avro.UBIEvent;
+import com.ebay.app.raptor.chocolate.avro.BehaviorEvent;
 import com.ebay.traffic.chocolate.flink.nrt.constant.DateConstants;
 import com.ebay.traffic.chocolate.flink.nrt.constant.PropertyConstants;
 import com.ebay.traffic.chocolate.flink.nrt.constant.StringConstants;
 import com.ebay.traffic.chocolate.flink.nrt.deserialization.DefaultKafkaDeserializationSchema;
 import com.ebay.traffic.chocolate.flink.nrt.function.ESMetricsCompatibleRichMapFunction;
+import com.ebay.traffic.chocolate.flink.nrt.transformer.BehaviorEventTransformer;
 import com.ebay.traffic.chocolate.flink.nrt.util.PropertyMgr;
-import com.ebay.traffic.monitoring.ESMetrics;
 import io.ebay.rheos.kafka.client.StreamConnectorConfig;
 import io.ebay.rheos.schema.avro.GenericRecordDomainDataDecoder;
 import io.ebay.rheos.schema.avro.RheosEventDeserializer;
 import io.ebay.rheos.schema.event.RheosEvent;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.util.Utf8;
-import org.apache.commons.lang3.Validate;
 import org.apache.flink.api.common.serialization.BulkWriter;
+import org.apache.flink.configuration.CheckpointingOptions;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.GlobalConfiguration;
+import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.core.io.SimpleVersionedSerializer;
 import org.apache.flink.formats.parquet.avro.ParquetAvroWriters;
@@ -32,31 +31,29 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public class UBIEventSinkApp extends AbstractRheosHDFSCompatibleApp<ConsumerRecord<byte[], byte[]>, UBIEvent> {
-  private static final Logger LOGGER = LoggerFactory.getLogger(UBIEventSinkApp.class);
+public class BehaviorEventSinkApp extends AbstractRheosHDFSCompatibleApp<ConsumerRecord<byte[], byte[]>, BehaviorEvent> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(BehaviorEventSinkApp.class);
 
   public static void main(String[] args) throws Exception {
-    UBIEventSinkApp transformApp = new UBIEventSinkApp();
-    transformApp.run();
+    BehaviorEventSinkApp sinkApp = new BehaviorEventSinkApp();
+    sinkApp.run();
   }
 
   @Override
   protected List<String> getConsumerTopics() {
     return  Arrays.asList(PropertyMgr.getInstance()
-                    .loadProperty(PropertyConstants.UBI_EVENT_SINK_APP_RHEOS_CONSUMER_TOPIC_PROPERTIES)
+                    .loadProperty(PropertyConstants.BEHAVIOR_EVENT_SINK_APP_RHEOS_CONSUMER_TOPIC_PROPERTIES)
                     .getProperty(PropertyConstants.TOPIC).split(StringConstants.COMMA));
   }
 
   @Override
   protected Properties getConsumerProperties() {
-    return PropertyMgr.getInstance().loadProperty(PropertyConstants.UBI_EVENT_SINK_APP_RHEOS_CONSUMER_PROPERTIES);
+    return PropertyMgr.getInstance().loadProperty(PropertyConstants.BEHAVIOR_EVENT_SINK_APP_RHEOS_CONSUMER_PROPERTIES);
   }
 
   @Override
@@ -66,29 +63,27 @@ public class UBIEventSinkApp extends AbstractRheosHDFSCompatibleApp<ConsumerReco
 
   @Override
   protected Path getSinkBasePath() {
-    Properties properties = PropertyMgr.getInstance().loadProperty(PropertyConstants.UBI_EVENT_SINK_APP_HDFS_PROPERTIES);
+    Properties properties = PropertyMgr.getInstance().loadProperty(PropertyConstants.BEHAVIOR_EVENT_SINK_APP_HDFS_PROPERTIES);
     return new Path(properties.getProperty(PropertyConstants.PATH));
   }
 
   @Override
-  protected StreamingFileSink<UBIEvent> getStreamingFileSink() {
+  protected StreamingFileSink<BehaviorEvent> getStreamingFileSink() {
     return StreamingFileSink.forBulkFormat(getSinkBasePath(), getSinkWriterFactory()).withBucketAssigner(getSinkBucketAssigner())
             .build();
   }
 
   @Override
-  protected BulkWriter.Factory<UBIEvent> getSinkWriterFactory() {
-    return ParquetAvroWriters.forSpecificRecord(UBIEvent.class);
+  protected BulkWriter.Factory<BehaviorEvent> getSinkWriterFactory() {
+    return ParquetAvroWriters.forSpecificRecord(BehaviorEvent.class);
   }
 
   @Override
-  protected DataStream<UBIEvent> transform(DataStreamSource<ConsumerRecord<byte[], byte[]>> dataStreamSource) {
+  protected DataStream<BehaviorEvent> transform(DataStreamSource<ConsumerRecord<byte[], byte[]>> dataStreamSource) {
     return dataStreamSource.map(new TransformRichMapFunction());
   }
 
-  protected static class TransformRichMapFunction extends ESMetricsCompatibleRichMapFunction<ConsumerRecord<byte[], byte[]>, UBIEvent> {
-    public static final String APPLICATION_PAYLOAD = "applicationPayload";
-    public static final String CLIENT_DATA = "clientData";
+  protected static class TransformRichMapFunction extends ESMetricsCompatibleRichMapFunction<ConsumerRecord<byte[], byte[]>, BehaviorEvent> {
     private transient GenericRecordDomainDataDecoder decoder;
     private transient RheosEventDeserializer deserializer;
 
@@ -98,52 +93,34 @@ public class UBIEventSinkApp extends AbstractRheosHDFSCompatibleApp<ConsumerReco
       deserializer = new RheosEventDeserializer();
       Map<String, Object> config = new HashMap<>();
       Properties properties = PropertyMgr.getInstance()
-              .loadProperty(PropertyConstants.UBI_EVENT_SINK_APP_RHEOS_CONSUMER_PROPERTIES);
+              .loadProperty(PropertyConstants.BEHAVIOR_EVENT_SINK_APP_RHEOS_CONSUMER_PROPERTIES);
       String rheosServiceUrl = properties.getProperty(StreamConnectorConfig.RHEOS_SERVICES_URLS);
       config.put(StreamConnectorConfig.RHEOS_SERVICES_URLS, rheosServiceUrl);
       decoder = new GenericRecordDomainDataDecoder(config);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public UBIEvent map(ConsumerRecord<byte[], byte[]> consumerRecord) throws Exception {
+    public BehaviorEvent map(ConsumerRecord<byte[], byte[]> consumerRecord) throws Exception {
       RheosEvent rheosEvent = deserializer.deserialize(consumerRecord.topic(), consumerRecord.value());
       GenericRecord sourceRecord = decoder.decode(rheosEvent);
-      UBIEvent ubiEvent = new UBIEvent();
-      for (Schema.Field field : UBIEvent.getClassSchema().getFields()) {
-        String name = field.name();
-        Object value = sourceRecord.get(name);
-        if (name.equals(APPLICATION_PAYLOAD) || name.equals(CLIENT_DATA)) {
-          ubiEvent.put(name, convertMap((HashMap<Utf8, Utf8>) value));
-          continue;
-        }
-        if (value instanceof Utf8) {
-          ubiEvent.put(name, String.valueOf(value));
-          continue;
-        }
-        ubiEvent.put(name, value);
-      }
-      return ubiEvent;
-    }
-
-    private String convertMap(HashMap<Utf8, Utf8> value) {
-      StringJoiner joiner = new StringJoiner(StringConstants.AND);
-      value.forEach((k, v) -> joiner.add(k + StringConstants.EQUAL + v));
-      return joiner.toString();
+      BehaviorEventTransformer behaviorEventTransformer = new BehaviorEventTransformer(sourceRecord);
+      BehaviorEvent behaviorEvent = new BehaviorEvent();
+      behaviorEventTransformer.transform(behaviorEvent);
+      return behaviorEvent;
     }
   }
 
   @Override
-  protected BucketAssigner<UBIEvent, String> getSinkBucketAssigner() {
+  protected BucketAssigner<BehaviorEvent, String> getSinkBucketAssigner() {
     return new CustomEventDateTimeBucketAssigner();
   }
 
-  private static class CustomEventDateTimeBucketAssigner implements BucketAssigner<UBIEvent, String> {
+  private static class CustomEventDateTimeBucketAssigner implements BucketAssigner<BehaviorEvent, String> {
     public static final DateTimeFormatter EVENT_DT_FORMATTER = DateTimeFormatter.ofPattern(DateConstants.YYYY_MM_DD).withZone(ZoneId.systemDefault());
 
     @Override
-    public String getBucketId(UBIEvent element, Context context) {
-      return StringConstants.BUCKET_PREFIX + EVENT_DT_FORMATTER.format(Instant.ofEpochMilli(element.getEventTimestamp()));
+    public String getBucketId(BehaviorEvent element, Context context) {
+      return StringConstants.BUCKET_PREFIX + EVENT_DT_FORMATTER.format(Instant.ofEpochMilli(element.getEventtimestamp()));
     }
 
     @Override
--- flink-nrt/src/test/java/com/ebay/traffic/chocolate/flink/nrt/app/BehaviorEventSinkAppTest.java
+++ flink-nrt/src/test/java/com/ebay/traffic/chocolate/flink/nrt/app/BehaviorEventSinkAppTest.java
@@ -1,6 +1,6 @@
 package com.ebay.traffic.chocolate.flink.nrt.app;
 
-import com.ebay.app.raptor.chocolate.avro.UBIEvent;
+import com.ebay.app.raptor.chocolate.avro.BehaviorEvent;
 import com.ebay.traffic.chocolate.flink.nrt.util.PropertyMgr;
 import io.ebay.rheos.kafka.client.StreamConnectorConfig;
 import io.ebay.rheos.schema.avro.SchemaRegistryAwareAvroSerializerHelper;
@@ -11,7 +11,6 @@ import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.*;
-import org.apache.avro.util.Utf8;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.functions.sink.filesystem.BucketAssigner;
 import org.apache.flink.streaming.api.operators.StreamMap;
@@ -31,29 +30,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
-public class UBIEventSinkAppTest {
-  private UBIEventSinkApp ubiEventSinkApp;
+public class BehaviorEventSinkAppTest {
+  private BehaviorEventSinkApp behaviorEventSinkApp;
 
   @Before
   public void setUp() throws Exception {
-    ubiEventSinkApp = new UBIEventSinkApp();
+    behaviorEventSinkApp = new BehaviorEventSinkApp();
   }
 
   @Test
   public void getConsumerTopics() {
-    assertEquals(Collections.singletonList("marketing.tracking.staging.behavior"), ubiEventSinkApp.getConsumerTopics());
+    assertEquals(Collections.singletonList("marketing.tracking.staging.behavior"), behaviorEventSinkApp.getConsumerTopics());
   }
 
   @Test
   public void getConsumerProperties() {
     Properties properties = new Properties();
     properties.setProperty("bootstrap.servers", "rhs-gvrvkiaa-kfk-slc-1.rheos-streaming-qa.svc.33.tess.io:9092,rhs-gvrvkiaa-kfk-slc-2.rheos-streaming-qa.svc.33.tess.io:9092,rhs-gvrvkiaa-kfk-slc-3.rheos-streaming-qa.svc.33.tess.io:9092,rhs-gvrvkiaa-kfk-slc-4.rheos-streaming-qa.svc.33.tess.io:9092");
-    properties.setProperty("group.id", "marketing-tracking-consumer-ubi-event-sink");
+    properties.setProperty("group.id", "marketing-tracking-consumer-behavior-event-sink");
     properties.setProperty("session.timeout.ms", "10000");
     properties.setProperty("auto.offset.reset", "earliest");
     properties.setProperty("receive.buffer.bytes", "65536");
@@ -62,19 +59,19 @@ public class UBIEventSinkAppTest {
     properties.setProperty("security.protocol", "SASL_PLAINTEXT");
     properties.setProperty("sasl.jaas.config", "io.ebay.rheos.kafka.security.iaf.IAFLoginModule required iafConsumerId=\"urn:ebay-marketplace-consumerid:0a2563dc-390f-4b78-8648-68c01e248639\" iafSecret=\"f660cc36-bf60-4528-befc-bb2fc203a960\" iafEnv=\"staging\";");
     properties.setProperty("rheos.services.urls", "https://rheos-services.qa.ebay.com");
-    assertEquals(properties, ubiEventSinkApp.getConsumerProperties());
+    assertEquals(properties, behaviorEventSinkApp.getConsumerProperties());
   }
 
   @Test
   public void getSinkBasePath() {
-    assertEquals(new Path("~/Downloads/flink-test/ubi-event-sink"), ubiEventSinkApp.getSinkBasePath());
+    assertEquals(new Path("~/Downloads/flink-test/behavior-event-sink"), behaviorEventSinkApp.getSinkBasePath());
   }
 
   @SuppressWarnings("unchecked")
   @Test
   public void transformRichMapFunction() throws Exception {
-    UBIEventSinkApp.TransformRichMapFunction transformRichMapFunction = new UBIEventSinkApp.TransformRichMapFunction();
-    OneInputStreamOperatorTestHarness<ConsumerRecord<byte[], byte[]>, UBIEvent> testHarness = new OneInputStreamOperatorTestHarness<>(new StreamMap<>(transformRichMapFunction));
+    BehaviorEventSinkApp.TransformRichMapFunction transformRichMapFunction = new BehaviorEventSinkApp.TransformRichMapFunction();
+    OneInputStreamOperatorTestHarness<ConsumerRecord<byte[], byte[]>, BehaviorEvent> testHarness = new OneInputStreamOperatorTestHarness<>(new StreamMap<>(transformRichMapFunction));
     testHarness.open();
 
     String json = PropertyMgr.getInstance().loadFile("email_open.json");
@@ -88,25 +85,42 @@ public class UBIEventSinkAppTest {
 
     ConcurrentLinkedQueue<Object> output = testHarness.getOutput();
     assertEquals(1, output.size());
-    StreamRecord<UBIEvent> streamRecord = (StreamRecord<UBIEvent>) output.poll();
+    StreamRecord<BehaviorEvent> streamRecord = (StreamRecord<BehaviorEvent>) output.poll();
     assertNotNull(streamRecord);
-    UBIEvent targetRecord = streamRecord.getValue();
-    for (Schema.Field field : targetRecord.getSchema().getFields()) {
-      String name = field.name();
-      Object source = sourceRecord.get(name);
-      Object target = targetRecord.get(name);
-      if (source instanceof Utf8) {
-        assertEquals(target, String.valueOf(source));
-        continue;
-      }
-      if (source instanceof HashMap) {
-        HashMap<Utf8, Utf8> source1 = (HashMap<Utf8, Utf8>) source;
-        assertEquals(target, source1.entrySet().stream()
-                .map(e -> e.getKey() + "=" + e.getValue()).collect(Collectors.joining("&")));
-        continue;
-      }
-      assertEquals(target, source);
-    }
+    BehaviorEvent behaviorEvent = streamRecord.getValue();
+    assertEquals("", behaviorEvent.getGuid());
+    assertEquals("acd12d5b79ac46b7b5eef0d76510cb1e", behaviorEvent.getAdguid());
+    assertEquals(Long.valueOf(6694873385117159424L), behaviorEvent.getSessionskey());
+    assertEquals(Long.valueOf(6694873385117159424L), behaviorEvent.getSnapshotid());
+    assertEquals(Integer.valueOf(1), behaviorEvent.getSeqnum());
+    assertEquals(Integer.valueOf(0), behaviorEvent.getSiteid());
+    assertEquals(Integer.valueOf(3962), behaviorEvent.getPageid());
+    assertEquals("impression", behaviorEvent.getPagename());
+    assertNull(behaviorEvent.getRefererhash());
+    assertEquals(Long.valueOf(1596182198056L), behaviorEvent.getEventtimestamp());
+    assertEquals("/marketingtracking/v1/impression?mkevt=4&mkcid=7&mkpid=0&sojTags=bu=bu&bu=43551630917&emsid=e11051.m44.l1139&euid=c527526a795a414cb4ad11bfaba21b5d&ext=56623",
+            behaviorEvent.getUrlquerystring());
+    assertEquals("Script=marketingtracking&Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit" +
+                    "/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36,GingerClient/0.11.0-RELEASE&" +
+                    "Server=mktcollectionsvc.vip.ebay.com&RemoteIP=10.249.75.9&ContentLength=211&TName=impression" +
+                    "&AcceptEncoding=gzip, deflate, br&ForwardedFor=10.249.75.9&TMachine=10.249.75.54",
+            behaviorEvent.getClientdata());
+    assertEquals("ext=56623&Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 " +
+                    "(KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36&" +
+                    "bu=43551630917&euid=c527526a795a414cb4ad11bfaba21b5d&emsid=e11051.m44.l1139" +
+                    "&Payload=/marketingtracking/v1/impression?mkevt=4&mkcid=7&mkpid=0&sojTags=bu=bu&bu=43551630917" +
+                    "&emsid=e11051.m44.l1139&euid=c527526a795a414cb4ad11bfaba21b5d&ext=56623" +
+                    "&emid=43551630917",
+            behaviorEvent.getApplicationpayload());
+    assertEquals("mktcollectionsvc.vip.ebay.com", behaviorEvent.getWebserver());
+    assertNull(behaviorEvent.getReferrer());
+    assertEquals("43551630917", behaviorEvent.getUserid());
+    assertEquals(Integer.valueOf(0), behaviorEvent.getRdt());
+    assertEquals("SITE_EMAIL", behaviorEvent.getChanneltype());
+    assertEquals("EMAIL_OEPN", behaviorEvent.getChannelaction());
+    assertEquals("", behaviorEvent.getDispatchid());
+    assertEquals(1, behaviorEvent.getData().size());
+    assertTrue(behaviorEvent.getData().iterator().next().isEmpty());
   }
 
   private GenericRecord createSourceRecord(String json) throws IOException {
@@ -116,7 +130,7 @@ public class UBIEventSinkAppTest {
     SchemaRegistryAwareAvroSerializerHelper<GenericRecord> serializerHelper =
             new SchemaRegistryAwareAvroSerializerHelper<>(map, GenericRecord.class);
 
-    Schema schema = serializerHelper.getSchema(9366);
+    Schema schema = serializerHelper.getSchema("marketing.tracking.behavior.schema");
 
     DecoderFactory decoderFactory = new DecoderFactory();
     Decoder decoder = decoderFactory.jsonDecoder(schema, json);
@@ -155,10 +169,10 @@ public class UBIEventSinkAppTest {
 
   @Test
   public void getSinkBucketAssigner() {
-    BucketAssigner<UBIEvent, String> sinkBucketAssigner = ubiEventSinkApp.getSinkBucketAssigner();
+    BucketAssigner<BehaviorEvent, String> sinkBucketAssigner = behaviorEventSinkApp.getSinkBucketAssigner();
     ZonedDateTime of = ZonedDateTime.of(2020, 8, 4, 7, 30, 0, 0, ZoneId.systemDefault());
-    UBIEvent element = Mockito.mock(UBIEvent.class);
-    Mockito.when(element.getEventTimestamp()).thenReturn(of.toInstant().toEpochMilli());
+    BehaviorEvent element = Mockito.mock(BehaviorEvent.class);
+    Mockito.when(element.getEventtimestamp()).thenReturn(of.toInstant().toEpochMilli());
     BucketAssigner.Context context = Mockito.mock(BucketAssigner.Context.class);
     assertEquals("dt=2020-08-04", sinkBucketAssigner.getBucketId(element, context));
   }
