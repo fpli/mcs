032dec5293f4d6da563befdbcceb6cb20d9a5007
Yang <yyang28@ebay.com>
2020-11-27 12:24:37 +0800

    update cast test.

--- adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/component/GdprConsentHandlerTest.java
+++ adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/component/GdprConsentHandlerTest.java
@@ -1,6 +1,8 @@
 package com.ebay.app.raptor.chocolate.adservice.component;
 
+import com.ebay.app.raptor.chocolate.adservice.util.CouchbaseClient;
 import com.ebay.app.raptor.chocolate.avro.HttpMethod;
+import com.ebay.app.raptor.chocolate.constant.CouchbaseKeyConstant;
 import com.ebay.app.raptor.chocolate.model.GdprConsentDomain;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,10 +20,13 @@ public class GdprConsentHandlerTest {
 
     @Test
     public void testPurpose123456ForAr() {
-        String targetUrl = "http://localhost:8080/marketingtracking/v1/ar?mpt=682877911&ff18=mWeb&siteid=0&icep_siteid=0&ipn=admain2&adtype=3&size=300x250&pgroup=532260&gdpr=1&gdpr_consent=CO9HbRYO9HbRYMEAAAENAwCAAPwAAAAAAAAAAAAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g";
+        CouchbaseClient instance = CouchbaseClient.getInstance();
+        instance.put(CouchbaseKeyConstant.ENABLE_TCF_COMPLIANCE_MODE, "true", 0);
+        instance.put(CouchbaseKeyConstant.PURPOSE_VENDOR_ID, "[22]", 0);
+        String targetUrl = "http://localhost:8080/marketingtracking/v1/ar?mpt=682877911&ff18=mWeb&siteid=0&icep_siteid=0&ipn=admain2&adtype=3&size=300x250&pgroup=532260&gdpr=1&gdpr_consent=CO9el_uO9el_uHrAAAENAwCAAPwAAAAAAAAAALAAABAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g";
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest(HttpMethod.GET.name(), targetUrl);
         mockHttpServletRequest.setParameter("gdpr","1");
-        mockHttpServletRequest.setParameter("gdpr_consent","CO9HbRYO9HbRYMEAAAENAwCAAPwAAAAAAAAAAAAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g");
+        mockHttpServletRequest.setParameter("gdpr_consent","CO9el_uO9el_uHrAAAENAwCAAPwAAAAAAAAAALAAABAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g");
         GdprConsentDomain gdprConsentDomain = gdprConsentHandler.handleGdprConsent(mockHttpServletRequest);
         assert gdprConsentDomain.isAllowedUseGeoInfo();
         assert gdprConsentDomain.isAllowedSetCookie();
@@ -30,10 +35,13 @@ public class GdprConsentHandlerTest {
 
     @Test
     public void testPurpose1ForAr() {
-        String targetUrl = "http://localhost:8080/marketingtracking/v1/ar?mpt=682877911&ff18=mWeb&siteid=0&icep_siteid=0&ipn=admain2&adtype=3&size=300x250&pgroup=532260&gdpr=1&gdpr_consent=CO9Xx5qO9Xx5qOsAAAENAwCAAIAAAB4AAAAAAGhV4BkAV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g";
+        CouchbaseClient instance = CouchbaseClient.getInstance();
+        instance.put(CouchbaseKeyConstant.ENABLE_TCF_COMPLIANCE_MODE, "true", 0);
+        instance.put(CouchbaseKeyConstant.PURPOSE_VENDOR_ID, "[22]", 0);
+        String targetUrl = "http://localhost:8080/marketingtracking/v1/ar?mpt=682877911&ff18=mWeb&siteid=0&icep_siteid=0&ipn=admain2&adtype=3&size=300x250&pgroup=532260&gdpr=1&gdpr_consent=CO9hOUaO9hOUaL0AAAENAwCAAIAAAAAAAAAAALAAABAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g";
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest(HttpMethod.GET.name(), targetUrl);
         mockHttpServletRequest.setParameter("gdpr","1");
-        mockHttpServletRequest.setParameter("gdpr_consent","CO9Xx5qO9Xx5qOsAAAENAwCAAIAAAB4AAAAAAGhV4BkAV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g");
+        mockHttpServletRequest.setParameter("gdpr_consent","CO9hOUaO9hOUaL0AAAENAwCAAIAAAAAAAAAAALAAABAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g");
         GdprConsentDomain gdprConsentDomain = gdprConsentHandler.handleGdprConsent(mockHttpServletRequest);
         assert !gdprConsentDomain.isAllowedUseGeoInfo();
         assert !gdprConsentDomain.isAllowedSetCookie();
--- event-listener/src/test/java/com/ebay/app/raptor/chocolate/eventlistener/component/GdprConsentHandlerTest.java
+++ event-listener/src/test/java/com/ebay/app/raptor/chocolate/eventlistener/component/GdprConsentHandlerTest.java
@@ -1,6 +1,8 @@
 package com.ebay.app.raptor.chocolate.eventlistener.component;
 
 import com.ebay.app.raptor.chocolate.constant.ChannelIdEnum;
+import com.ebay.app.raptor.chocolate.constant.CouchbaseKeyConstant;
+import com.ebay.app.raptor.chocolate.eventlistener.util.CouchbaseClient;
 import com.ebay.app.raptor.chocolate.model.GdprConsentDomain;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +20,9 @@ public class GdprConsentHandlerTest {
 
     @Test
     public void testCanStorePersonalizedAndContextual() {
+        CouchbaseClient instance = CouchbaseClient.getInstance();
+        instance.put(CouchbaseKeyConstant.ENABLE_TCF_COMPLIANCE_MODE, "true", 0);
+        instance.put(CouchbaseKeyConstant.PURPOSE_VENDOR_ID, "[22]", 0);
         String targetUrl = "http://www.ebayadservices.com/marketingtracking/v1/ar?mpt=682877911&ff18=mWeb&siteid=0&icep_siteid=0&ipn=admain2&adtype=3&size=300x250&pgroup=532260&gdpr=1&gdpr_consent=CO4REdoO4REdoAOACBENBACoAP%5FAAH%5FAACiQGVtd%5FX9fb2tj%2D%5F599%5Ft0eY1f9%5F63t2wzjgeNs%2D8NyZ%5FX%5FJ4Xv2MyvA34pqYKmR4kunbBAQFtHGncTQgBwIlVqTLsYk2MjzNKJ7JEilsbe2dYGHtPn8VT%2DZCZr06s%5F%5F%5F7v3%5F%5F%5F%5F%5F%5F7oGUEEmGpfAQJCWMBJNmlUKIEIVxIVAGACihGFo0sNCRwU7I4CPUACAAAYgIQIgQYgohZBAAAAAElEQAgAwIBEARAIAAQAjQEIACJAEFgBIGAQACoGhYARRBCBIQYHBUcogQFSLRQTzAAA";
         GdprConsentDomain gdprConsentDomain = gdprConsentHandler.handleGdprConsent(targetUrl, ChannelIdEnum.DAP);
         assert gdprConsentDomain.isAllowedStoredContextualData();
@@ -26,6 +31,9 @@ public class GdprConsentHandlerTest {
 
     @Test
     public void testPurpose123456() {
+        CouchbaseClient instance = CouchbaseClient.getInstance();
+        instance.put(CouchbaseKeyConstant.ENABLE_TCF_COMPLIANCE_MODE, "true", 0);
+        instance.put(CouchbaseKeyConstant.PURPOSE_VENDOR_ID, "[22]", 0);
         String targetUrl = "http://www.ebayadservices.com/marketingtracking/v1/ar?mpt=682877911&ff18=mWeb&siteid=0&icep_siteid=0&ipn=admain2&adtype=3&size=300x250&pgroup=532260&gdpr=1&gdpr_consent=CO9HbRYO9HbRYMEAAAENAwCAAPwAAAAAAAAAAAAAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g";
         GdprConsentDomain gdprConsentDomain = gdprConsentHandler.handleGdprConsent(targetUrl, ChannelIdEnum.DAP);
         assert !gdprConsentDomain.isAllowedStoredContextualData();
