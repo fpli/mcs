f358e3c8f270e38940c18b5a27b6e0c20f4b3d2b
Yang <yyang28@ebay.com>
2020-11-30 15:07:18 +0800

    add dependsOn for gdprconsenthandler

--- adservice/src/main/java/com/ebay/app/raptor/chocolate/adservice/component/GdprConsentHandler.java
+++ adservice/src/main/java/com/ebay/app/raptor/chocolate/adservice/component/GdprConsentHandler.java
@@ -16,6 +16,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +32,7 @@ import java.util.stream.Collectors;
  * @author yyang28
  */
 @Component
+@DependsOn("AdserviceService")
 public class GdprConsentHandler {
     private static final String gdprParameter = "gdpr";
     private static final String gdprConsentParameter = "gdpr_consent";
