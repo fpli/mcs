7da1244dd1d222d07ed18dfc8ee7d781b3707702
Yang <yyang28@ebay.com>
2020-12-09 16:38:03 +0800

    remove useless UT and import.

--- adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/util/DAPResponseHandlerTest.java
+++ adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/util/DAPResponseHandlerTest.java
@@ -66,38 +66,6 @@ public class DAPResponseHandlerTest {
   }
 
   @Test
-  public void getSiteId() throws Exception {
-    Client client = Mockito.mock(Client.class);
-    Configuration conf = Mockito.mock(Configuration.class);
-    Response response = prepareResponse();
-    WebTarget webTarget = Mockito.mock(WebTarget.class);
-    Invocation.Builder builder = Mockito.mock(Invocation.Builder.class);
-
-    when(conf.getProperty(EndpointUri.KEY)).thenReturn("localhost");
-    when(builder.get()).thenReturn(response);
-    when(webTarget.queryParam(anyString(), anyString())).thenReturn(webTarget);
-    when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
-    when(client.target(anyString())).thenReturn(webTarget);
-    when(client.getConfiguration()).thenReturn(conf);
-
-    lbsClient = LBSClient.getInstance();
-    Whitebox.setInternalState(lbsClient, "client", client);
-
-    LBSQueryResult lbsQueryResult = lbsClient.getLBSInfo("97.77.104.22");
-    assertEquals(Integer.valueOf(0), Whitebox.<Integer>invokeMethod(dapResponseHandler, "getSiteId", lbsQueryResult));
-  }
-
-  @Test
-  public void setSiteId() throws Exception {
-    URIBuilder dapUriBuilder = new URIBuilder();
-    Whitebox.<String>invokeMethod(dapResponseHandler, "setSiteId", dapUriBuilder, 2);
-    List<NameValuePair> collect = dapUriBuilder.getQueryParams().stream()
-        .filter(nameValuePair -> nameValuePair.getName().equals(Constants.SITE_ID))
-        .collect(Collectors.toList());
-    assertEquals("2", collect.get(0).getValue());
-  }
-
-  @Test
   public void getUaPrime() throws Exception {
     Map<String, String[]> map = new HashMap<>();
     assertNull(Whitebox.<Integer>invokeMethod(dapResponseHandler, "getUaPrime", map));
