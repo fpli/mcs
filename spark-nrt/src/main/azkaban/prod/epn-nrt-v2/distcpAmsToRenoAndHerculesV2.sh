4da11561ea8c1125f08f8843a4b9297e8dd2d355
yuhxiao <yuhxiao@ebay.com>
2021-02-02 15:51:24 +0800

    epnnrtv2_history

--- spark-nrt/src/main/azkaban/prod/epn-nrt-v2/distcpAmsToRenoAndHerculesV2.sh
+++ spark-nrt/src/main/azkaban/prod/epn-nrt-v2/distcpAmsToRenoAndHerculesV2.sh
@@ -6,12 +6,12 @@ function start_job(){
     host=$1
     ssh -o ServerAliveInterval=180 -o ServerAliveCountMax=10 -T -i /usr/azkaban/id_rsa_spark _choco_admin@${host} <<EOSSH
     hostname
-    cd /datashare/mkttracking/jobs/tracking/epnnrt/bin/prod
+    cd /datashare/mkttracking/jobs/tracking/epnnrt_v2/bin/prod
     pwd
     export HADOOP_USER_NAME=chocolate
     echo $HADOOP_USER_NAME
-    ./distcpAmsToRenoAndHercules.sh /apps/epn-nrt/click_v2 /apps/b_marketing_tracking/chocolate/epnnrt/click_v2 /sys/edw/imk/im_tracking/epn/ams_click_v2/snapshot click &&
-    ./distcpAmsToRenoAndHercules.sh /apps/epn-nrt/impression_v2 /apps/b_marketing_tracking/chocolate/epnnrt/imprsn_v2 /sys/edw/imk/im_tracking/epn/ams_impression_v2/snapshot imp
+    ./distcpAmsToRenoAndHercules.sh /apps/epn-nrt/click_v2 /apps/b_marketing_tracking/chocolate/epnnrt_v2/click /sys/edw/imk/im_tracking/epn/ams_click_v2/snapshot click &&
+    ./distcpAmsToRenoAndHercules.sh /apps/epn-nrt/impression_v2 /apps/b_marketing_tracking/chocolate/epnnrt_v2/imp /sys/edw/imk/im_tracking/epn/ams_impression_v2/snapshot imp
 EOSSH
 }
 
--- spark-nrt/src/main/azkaban/prod/epn-nrt-v2/epnnrt_click_v2-scheduler.sh
+++ spark-nrt/src/main/azkaban/prod/epn-nrt-v2/epnnrt_click_v2-scheduler.sh
@@ -41,4 +41,4 @@ echo "FILTER_TIME="${FILTER_TIME} | tee -a ${log_file}
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
 #/datashare/mkttracking/jobs/tracking/epnnrt/bin/prod/epnnrt.sh ${WORK_DIR} ${RESOURCE_DIR} ${FILTER_TIME}
-/datashare/mkttracking/jobs/tracking/epnnrt/bin/prod_v2/epnnrt_click_v2.sh ${WORK_DIR} ${RESOURCE_DIR} "0"
+/datashare/mkttracking/jobs/tracking/epnnrt_v2/bin/prod/epnnrt_click_v2.sh ${WORK_DIR} ${RESOURCE_DIR} "0"
