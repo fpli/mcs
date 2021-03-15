72faab286bf97d7de338e1246020b86362c14e50
zhiyuawang <zhiyuawang@ebay.com>
2021-03-15 14:40:15 +0800

    update flink deploy script

--- flink-nrt/deploy.sh
+++ flink-nrt/deploy.sh
@@ -2,11 +2,8 @@
 
 set -x
 
-USERNAME=${1}
-PASSWORD=${2}
-
 CUR_MIN=$(date +%Y%m%d%H%M)
 SHORT_GIT_COMMIT=$(git rev-parse --short HEAD)
 CUR_VERSION="${CUR_MIN}-${SHORT_GIT_COMMIT}"
 
-mvn -DskipTests=true clean package -Dcommit.id=${CUR_VERSION} job-uploader:upload -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Dnamespace=marketing-tracking -DjobJarName=chocolate-flink-nrt -DjobJarTag=2.0-SNAPSHOT-${CUR_VERSION}
+mvn -DskipTests=true clean package -Dcommit.id=${CUR_VERSION} job-uploader:upload -Dusername=${1} -Dpassword=${2} -Dnamespace=marketing-tracking -DjobJarName=chocolate-flink-nrt -DjobJarTag=2.0-SNAPSHOT-${CUR_VERSION}
