eb4ab66eb141288f9cc99d861a582cc3ef9f92b9
yuhxiao <yuhxiao@ebay.com>
2021-04-19 17:42:16 +0800

    epnnrt_parquet

--- tools/AlertingAggrate-tool/bin/EpnNrtAutomationTest/epnnrt_click_new_test-scheduler.sh
+++ tools/AlertingAggrate-tool/bin/EpnNrtAutomationTest/epnnrt_click_new_test-scheduler.sh
@@ -8,14 +8,13 @@ bin=`cd "$bin">/dev/null; pwd`
 ##################### SPARK NRT Job Parameters ##################
 INPUT_WORK_DIR=hdfs://slickha/apps/tracking-events-workdir-new-test
 OUTPUT_WORK_DIR=hdfs://slickha/apps/tracking-events-workdir-new-test
-
 RESOURCE_DIR=/apps/tracking-resources-new-test
 
 OUTPUT_DIR=hdfs://slickha/apps/epn-nrt-new-test
-
 FILTER_HOUR=1     # 1:00am
 log_dt=${HOSTNAME}_$(date +%Y%m%d%H%M%S)
 log_file="/datashare/mkttracking/logs/chocolate/epn-nrt-new-test/scheduler_${log_dt}.log"
+date=`date -d '2 days ago' +%Y-%m-%d`
 
 ##################### Generate FILTER_TIME ##################
 HOUR=$(date +%_H)
@@ -40,6 +39,6 @@ echo "FILTER_TIME="${FILTER_TIME} | tee -a ${log_file}
 ##################### Spark Submit ##################
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
-/datashare/mkttracking/jobs/tracking/epnnrt_automation/bin/prod/epnnrt_click_automation_test.sh ${INPUT_WORK_DIR} ${OUTPUT_WORK_DIR} ${RESOURCE_DIR} "0" ${OUTPUT_DIR}
-/datashare/mkttracking/jobs/tracking/epnnrt_automation/bin/prod/distcpAmsToRenoForAutomation.sh /apps/epn-nrt-new-test/click /apps/b_marketing_tracking/chocolate/epnnrt-new-test/click click 2021-04-09
+/datashare/mkttracking/jobs/tracking/epnnrt_new_test/bin/prod/epnnrt_click_automation_test.sh ${INPUT_WORK_DIR} ${OUTPUT_WORK_DIR} ${RESOURCE_DIR} "0" ${OUTPUT_DIR}
+/datashare/mkttracking/jobs/tracking/epnnrt_new_test/bin/prod/distcpAmsToRenoForAutomation.sh /apps/epn-nrt-new-test/click /apps/b_marketing_tracking/chocolate/epnnrt-new-test/click click ${date}
 
--- tools/AlertingAggrate-tool/bin/EpnNrtAutomationTest/epnnrt_click_old_test-scheduler.sh
+++ tools/AlertingAggrate-tool/bin/EpnNrtAutomationTest/epnnrt_click_old_test-scheduler.sh
@@ -41,5 +41,5 @@ echo "FILTER_TIME="${FILTER_TIME} | tee -a ${log_file}
 ##################### Spark Submit ##################
 export HADOOP_USER_NAME=chocolate
 echo $HADOOP_USER_NAME
-/datashare/mkttracking/jobs/tracking/epnnrt_v2/bin/prod/epnnrt_click_automation_test.sh ${INPUT_WORK_DIR} ${OUTPUT_WORK_DIR} ${RESOURCE_DIR} "0" ${OUTPUT_DIR}
-/datashare/mkttracking/jobs/tracking/epnnrt_v2/bin/prod/distcpAmsToRenoForAutomation.sh /apps/epn-nrt-old-test/click /apps/b_marketing_tracking/chocolate/epnnrt-old-test/click click 2021-04-09
+/datashare/mkttracking/jobs/tracking/epnnrt_old_test/bin/prod/epnnrt_click_automation_test.sh ${INPUT_WORK_DIR} ${OUTPUT_WORK_DIR} ${RESOURCE_DIR} "0" ${OUTPUT_DIR}
+/datashare/mkttracking/jobs/tracking/epnnrt_old_test/bin/prod/distcpAmsToRenoForAutomation.sh /apps/epn-nrt-old-test/click /apps/b_marketing_tracking/chocolate/epnnrt-old-test/click click 2021-04-09
