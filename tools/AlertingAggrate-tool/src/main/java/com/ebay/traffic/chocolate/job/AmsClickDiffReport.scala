0ea8323603d5cd537c1e543f54088fbb671e6f2d
yuhxiao <yuhxiao@ebay.com>
2021-07-19 16:12:27 +0800

    Tess migration

--- tools/AlertingAggrate-tool/src/main/java/com/ebay/traffic/chocolate/job/AmsClickDiffReport.scala
+++ tools/AlertingAggrate-tool/src/main/java/com/ebay/traffic/chocolate/job/AmsClickDiffReport.scala
@@ -4,8 +4,10 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{DataFrame,SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.slf4j.LoggerFactory
+
+import scala.collection.mutable
 
 object AmsClickDiffReport extends App {
 
@@ -13,18 +15,18 @@ object AmsClickDiffReport extends App {
     if (args.length < 5) {
       println("Wrong arguments")
     }
-    val newClickInputPath:String=args(0)
-    val oldClickInputPath:String=args(1)
-    val outputPath:String = args(2)
-    val mode: String = args(3)
+    val clickDt: String=args(0)
+    val outputPath:String = args(1)
+    val mode: String = args(2)
 
-    val job = new AmsClickDiffReport(newClickInputPath, oldClickInputPath,outputPath, mode)
+
+    val job = new AmsClickDiffReport(clickDt,outputPath, mode)
     job.run()
     job.stop()
   }
 
 }
-class AmsClickDiffReport(val newClickInputPath: String, val oldClickInputPath: String,val outputPath: String, val mode: String = "local[4]") extends Serializable {
+class AmsClickDiffReport(val clickDt:String,val outputPath: String, val mode: String = "local[4]") extends Serializable {
   @transient lazy val logger = LoggerFactory.getLogger(this.getClass)
 
   /**
@@ -99,17 +101,42 @@ class AmsClickDiffReport(val newClickInputPath: String, val oldClickInputPath: S
   }
 
   def generateClickDiffReport = {
-    val newClickCount:Long=spark.read
-      .parquet(newClickInputPath)
-      .count()
-    logger.info("new click count is {}",newClickCount)
-    val oldClickCount:Long=spark.read
-      .parquet(oldClickInputPath)
-      .count()
-    logger.info("old click count is {}",oldClickCount)
 
   }
-
+  def getTotalCount():(Long,Long)={
+    val newTotalCount:Long=sqlsc.sql("select count(*) from choco_data.ams_click_new_test where click_dt=\""+clickDt+"\"").head().getLong(0)
+    val oldTotalCount:Long=sqlsc.sql("select count(*) from choco_data.ams_click_old_test where click_dt=\""+clickDt+"\"").head().getLong(0)
+    (newTotalCount,oldTotalCount)
+  }
+  def getUserIdNotNullPercent:(Double, Double)={
+    val newUserIdNotNullPercent:Double=sqlsc.sql("select count(*) from choco_data.ams_click_new_test where click_dt=\""+clickDt+"\"and  lower(brwsr_name) not like '%bot%' and USER_ID <>0 AND USER_ID <>-1 AND  USER_ID IS NOT NULL )" +
+      "/(SELECT count(*) from choco_data.ams_click_new_test where click_dt=\""+clickDt+"\" and lower(brwsr_name) not like '%bot%'").head.getDouble(0)
+    val oldUserIdNotNullPercent:Double=sqlsc.sql("select count(*) from choco_data.ams_click_old_test where click_dt=\""+clickDt+"\"and  lower(brwsr_name) not like '%bot%' and USER_ID <>0 AND USER_ID <>-1 AND  USER_ID IS NOT NULL )" +
+      "/(SELECT count(*) from choco_data.ams_click_old_test where click_dt=\""+clickDt+"\" and lower(brwsr_name) not like '%bot%'").head.getDouble(0)
+    (newUserIdNotNullPercent,oldUserIdNotNullPercent)
+  }
+  def getLastVwdItemIdNotNullPercent:(Double, Double)={
+    val newLastVwdItemIdNotNullPercent:Double=sqlsc.sql("select count(*) from choco_data.ams_click_new_test where click_dt=\""+clickDt+"\"and  lower(brwsr_name) not like '%bot%' and LAST_VWD_ITEM_ID is not null )" +
+      "/(SELECT count(*) from choco_data.ams_click_new_test where click_dt=\""+clickDt+"\" and lower(brwsr_name) not like '%bot%'").head.getDouble(0)
+    val oldLastVwdItemIdNotNullPercent:Double=sqlsc.sql("select count(*) from choco_data.ams_click_old_test where click_dt=\""+clickDt+"\"and  lower(brwsr_name) not like '%bot%' and LAST_VWD_ITEM_ID is not null )" +
+      "/(SELECT count(*) from choco_data.ams_click_old_test where click_dt=\""+clickDt+"\" and lower(brwsr_name) not like '%bot%'").head.getDouble(0)
+    (newLastVwdItemIdNotNullPercent,oldLastVwdItemIdNotNullPercent)
+  }
+  def getDiffColumnsAndCount:mutable.HashMap[String,Long]={
+    val map=new mutable.HashMap[String,Long]()
+    val df: DataFrame =sqlsc.sql("select * from choco_data.epnnrt_click_automation_diff where click_dt=\""+clickDt+"\"")
+    val columns: Array[String] = df.columns
+    var index:Int=0
+    //diff every two columns(new and old), if they are different, add this column to map
+    while(index<columns.length) {
+      //the columns' name will be like new_click_id and old_click_id, 'trim' function is to get final column name
+      map.put(columns(index).substring(4), df.select(columns(index),columns(index+1))
+        .filter(col(columns(index))=!=col(columns(index+1)))
+        .count())
+      index+=2
+    }
+    map
+  }
   /**
     * stop the spark com.xl.traffic.chocolate.job.
     */
