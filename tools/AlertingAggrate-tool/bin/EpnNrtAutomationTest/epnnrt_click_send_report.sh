40273161951aa9385c897eece65f225ec109d627
yuhxiao <yuhxiao@ebay.com>
2021-08-13 11:00:45 +0800

    Tess migration

--- tools/AlertingAggrate-tool/bin/EpnNrtAutomationTest/epnnrt_click_send_report.sh
+++ tools/AlertingAggrate-tool/bin/EpnNrtAutomationTest/epnnrt_click_send_report.sh
@@ -3,8 +3,8 @@ export HADOOP_USER_NAME=chocolate
 RESULT_PATH=viewfs://apollo-rno//apps/b_marketing_tracking/chocolate/epnnrt-automation-report/click_result.txt
 SUBJECT="epnnrt click automation result"
 EMAIL_CONTENT=`/datashare/mkttracking/tools/apollo_rno/hadoop_apollo_rno/bin/hdfs dfs -text ${RESULT_PATH}`
-export mail_list='yuhxiao@ebay.com'
+export mail_list='Marketing-Tracking-oncall@ebay.com','DL-eBay-Chocolate-GC@ebay.com'
 export mail_smtp='mx.vip.lvs.ebay.com:25'
 export mail_type='message-content-type=html'
 export mail_fm='_choco_admin@slcchocolatepits-1242736.stratus.slc.ebay.com'
-echo -e "${EMAIL_CONTENT}" | ./sendEmail -u "$SUBJECT" -f $mail_fm  -t $mail_list -s $mail_smtp -o $mail_type message-charset=utf-8
+echo -e "${EMAIL_CONTENT}" | /datashare/mkttracking/jobs/tracking/epnnrt_new_test/bin/sendEmail -u "$SUBJECT" -f $mail_fm  -t $mail_list -s $mail_smtp -o $mail_type message-charset=utf-8
