dacf9887ac0818dc36700ffc3ca375d72ee9dd1b
yuhxiao <yuhxiao@ebay.com>
2021-08-26 17:53:25 +0800

    Tess migration

--- spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/epnnrtV2/EpnNrtCommonV2.scala
+++ spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/epnnrtV2/EpnNrtCommonV2.scala
@@ -1208,6 +1208,12 @@ class EpnNrtCommonV2(params: ParameterV2, df: DataFrame) extends Serializable {
     clickFilter_map = batchGetAdvClickFilterMap(publisher_list)
     pubDomain_map = batchGetPubDomainMap(publisher_list)
 
+    logger.error("automation get map publisher {}"+publisher_map)
+    logger.error("automation get map campaign {}"+campaign_map)
+    logger.error("automation get map prog {}"+prog_map)
+    logger.error("automation get map clickFilter {}"+clickFilter_map)
+    logger.error("automation get map pubDomain_map {}"+pubDomain_map)
+
     metrics.mean("NrtCouchbaseLatencyTess", System.currentTimeMillis() - start)
 
 
