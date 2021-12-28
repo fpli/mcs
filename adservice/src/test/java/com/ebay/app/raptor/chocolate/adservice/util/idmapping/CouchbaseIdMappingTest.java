979f85dd9396a8a57237f86b5b7b9363e0a7bd6a
jialili1 <jialili1@ebay.com>
2021-12-28 17:09:22 +0800

    fix ut (#1416)

--- adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/util/idmapping/CouchbaseIdMappingTest.java
+++ adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/util/idmapping/CouchbaseIdMappingTest.java
@@ -41,7 +41,7 @@ public class CouchbaseIdMappingTest {
     idMapping.addMapping("123", "456", "456", "12345");
     idMapping.addMapping("234", "", "", "12345");
     idMapping.addMapping("345", "456", "456",  "");
-    idMapping.addMapping("345", "456,789", "789", "12345");
+    idMapping.addMapping("345", "456&789", "789", "12345");
 
     assertEquals("456", idMapping.getGuidListByAdguid("123"));
     assertEquals("456", idMapping.getGuidByAdguid("123"));
@@ -51,7 +51,7 @@ public class CouchbaseIdMappingTest {
     assertEquals("", idMapping.getGuidByAdguid("234"));
     assertEquals("12345", idMapping.getUidByAdguid("234"));
 
-    assertEquals("456,789", idMapping.getGuidListByAdguid("345"));
+    assertEquals("456&789", idMapping.getGuidListByAdguid("345"));
     assertEquals("789", idMapping.getGuidByAdguid("345"));
     assertEquals("12345", idMapping.getUidByAdguid("345"));
 
--- adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/util/idmapping/LocalCacheIdMappingTest.java
+++ adservice/src/test/java/com/ebay/app/raptor/chocolate/adservice/util/idmapping/LocalCacheIdMappingTest.java
@@ -12,7 +12,7 @@ public class LocalCacheIdMappingTest {
     idMapping.addMapping("123", "456", "456", "12345");
     idMapping.addMapping("234", "", "", "12345");
     idMapping.addMapping("345", "456", "456",  "");
-    idMapping.addMapping("345", "456,789", "789", "12345");
+    idMapping.addMapping("345", "456&789", "789", "12345");
 
     assertEquals("456", idMapping.getGuidListByAdguid("123"));
     assertEquals("456", idMapping.getGuidByAdguid("123"));
@@ -22,7 +22,7 @@ public class LocalCacheIdMappingTest {
     assertEquals("", idMapping.getGuidByAdguid("234"));
     assertEquals("12345", idMapping.getUidByAdguid("234"));
 
-    assertEquals("456,789", idMapping.getGuidListByAdguid("345"));
+    assertEquals("456&789", idMapping.getGuidListByAdguid("345"));
     assertEquals("789", idMapping.getGuidByAdguid("345"));
     assertEquals("12345", idMapping.getUidByAdguid("345"));
 
