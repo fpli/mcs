8c5fe8bae853a2b142ee127e510d742274b9361d
jialili1 <jialili1@ebay.com>
2022-01-26 17:57:14 +0800

    Revert "add customized doneFile (#1421)" (#1442)
    This reverts commit a62ac878f88869636bdc7593f24f72d4ce72908f.

--- spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/hourlyDone/UTPHourlyDoneJob.scala
+++ spark-nrt/src/main/scala/com/ebay/traffic/chocolate/sparknrt/hourlyDone/UTPHourlyDoneJob.scala
@@ -7,17 +7,13 @@ package com.ebay.traffic.chocolate.sparknrt.hourlyDone
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.time.{Instant, ZoneId, ZonedDateTime}
-
 import com.ebay.traffic.chocolate.spark.BaseSparkJob
 import com.ebay.traffic.chocolate.spark.DataFrameFunctions._
 import com.ebay.traffic.chocolate.sparknrt.utils.Utils
 import com.ebay.traffic.chocolate.sparknrt.utils.Utils.simpleUid
 import org.apache.hadoop.fs.{FileStatus, Path}
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{Column, DataFrame}
 import org.apache.spark.sql.functions._
-import org.yaml.snakeyaml.Yaml
-
-import scala.collection.mutable
 
 object UTPHourlyDoneJob extends App {
   override def main(args: Array[String]): Unit = {
@@ -47,8 +43,6 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
   @transient lazy val cachePrefixDatetimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm").withZone(defaultZoneId)
   @transient lazy val dtFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(defaultZoneId)
   @transient lazy val datetimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(defaultZoneId)
-  @transient lazy val yaml = new Yaml()
-  @transient lazy val doneFileConfig: java.util.List[java.util.Map[String, String]] = yaml.load(getClass.getClassLoader.getResourceAsStream("done_file_config.yaml")).asInstanceOf[java.util.List[java.util.Map[String,String]]]
 
   implicit def dateTimeOrdering: Ordering[ZonedDateTime] = Ordering.fromLessThan(_ isBefore  _)
 
@@ -67,11 +61,11 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
     * @param fileStatus input file status
     * @return date time
     */
-  def getLastDoneFileDatetimeFromDoneFiles(fileStatus: Array[FileStatus], doneFilePrefix: String): ZonedDateTime = {
+  def getLastDoneFileDatetimeFromDoneFiles(fileStatus: Array[FileStatus]): ZonedDateTime = {
     fileStatus
       .map(status => status.getPath.getName)
       .filter(fileName => fileName.contains(doneFilePrefix))
-      .map(fileName => {
+      .map(fileName =>  {
         val str = fileName.substring(doneFilePrefix.length, fileName.length - doneFilePostfix.length)
         ZonedDateTime.parse(str, doneFileDatetimeFormatter)
       })
@@ -83,7 +77,6 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
     * @param dateTime input date time
     * @return last date time the table ever touched done file and the delay hours
     */
-    @Deprecated
   def getLastDoneFileDateTimeAndDelay(dateTime: ZonedDateTime, doneDir: String): (ZonedDateTime, Long) = {
     // the done if there is no delay,
     val idealDone = dateTime.truncatedTo(ChronoUnit.HOURS).minusHours(1)
@@ -102,68 +95,20 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
         .map(status => status.getPath.getName)
         .filter(fileName => fileName.contains(doneFilePrefix))
       if (!todayDoneFiles.isEmpty) {
-        lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(todayDoneDir), doneFilePrefix)
+        lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(todayDoneDir))
       } else {
         fs.mkdirs(todayDoneDir)
-        lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(yesterdayDoneDir), doneFilePrefix)
+        lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(yesterdayDoneDir))
       }
     } else {
       fs.mkdirs(todayDoneDir)
-      lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(yesterdayDoneDir), doneFilePrefix)
+      lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(yesterdayDoneDir))
     }
     delays = ChronoUnit.HOURS.between(lastDone, idealDone)
 
     logger.info("lastDone {}, delays {}", lastDone, delays)
 
     (lastDone, delays)
-  }
-
-  /**
-    * get each last done time , delay, condition by done_file_config
-    * @param dateTime
-    * @param doneDir
-    * @return
-    */
-  def getEachLastDone(dateTime: ZonedDateTime, doneDir: String): mutable.Map[String, (ZonedDateTime, Long)] = {
-    // the done if there is no delay,
-    val idealDone = dateTime.truncatedTo(ChronoUnit.HOURS).minusHours(1)
-
-    val todayDoneDir = new Path(getDoneDir(idealDone, doneDir))
-    val yesterdayDoneDir = new Path(getDoneDir(idealDone.minusDays(1),doneDir))
-
-    logger.info("idealDone {}, todayDoneDir {}, yesterdayDoneDir {}", idealDone, todayDoneDir, yesterdayDoneDir)
-
-    var lastDone: ZonedDateTime = idealDone
-    var delays = 0L
-
-    val iterator = doneFileConfig.iterator()
-    val result: mutable.Map[String, (ZonedDateTime, Long)] = mutable.Map()
-    while (iterator.hasNext) {
-      val doneConfig = iterator.next()
-      val prefix = doneConfig.get("doneFilePrefix")
-
-      // if today done file doesn't exist, check yesterday
-      if (fs.exists(todayDoneDir) && fs.listStatus(todayDoneDir).length != 0) {
-        val todayDoneFiles = fs.listStatus(todayDoneDir)
-          .map(status => status.getPath.getName)
-          .filter(fileName => {
-            fileName.contains(prefix)
-          })
-        if (!todayDoneFiles.isEmpty) {
-          lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(todayDoneDir), prefix)
-        } else {
-          fs.mkdirs(todayDoneDir)
-          lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(yesterdayDoneDir), prefix)
-        }
-      } else {
-        fs.mkdirs(todayDoneDir)
-        lastDone = getLastDoneFileDatetimeFromDoneFiles(fs.listStatus(yesterdayDoneDir), prefix)
-      }
-      delays = ChronoUnit.HOURS.between(lastDone, idealDone)
-      logger.info("doneName : {}, lastDone : {}, delays {}", doneConfig.get("name"), lastDone.toString, delays.toString)
-      result += (doneConfig.get("name") -> (lastDone, delays))
-    }
-    result
   }
 
   /**
@@ -225,10 +170,6 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
     getDoneDir(doneFileDatetime, doneFileDir) + "/" + doneFilePrefix + doneFileDatetime.format(doneFileDatetimeFormatter) + doneFilePostfix
   }
 
-  def getDoneFileNameByConfig(doneFileDatetime: ZonedDateTime, doneFileDir: String, prefix: String): String = {
-    getDoneDir(doneFileDatetime, doneFileDir) + "/" + prefix + doneFileDatetime.format(doneFileDatetimeFormatter) + doneFilePostfix
-  }
-
   private val datetimeUdf = udf((timestamp: Long) => datetime(timestamp))
 
   def datetime(timestamp: Long): String = {
@@ -241,12 +182,12 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
     * @param lastDoneAndDelay last done datetime and the delayed hours
     * @param inputDateTime input datetime, it should be now
     */
-    @Deprecated
   def generateHourlyDoneFile(diffDf: DataFrame, lastDoneAndDelay: (ZonedDateTime, Long), inputDateTime: ZonedDateTime): Option[ZonedDateTime] = {
     // generate done file
     if (diffDf.rdd.isEmpty) {
       return None
     }
+
     val stat = diffDf.groupBy("channelType", "actionType").agg(min("producerEventTs").as("minProducerEventTs"))
       .select("channelType", "actionType", "minProducerEventTs")
       .withColumn("minProducerEventTime", datetimeUdf(col("minProducerEventTs")))
@@ -275,79 +216,25 @@ class UTPHourlyDoneJob(params: Parameter, override val enableHiveSupport: Boolea
     Some(times.last)
   }
 
-  def genHourlyDoneFile(diffDf: DataFrame, lastDoneAndDelay: mutable.Map[String, (ZonedDateTime, Long)]): Option[ZonedDateTime] = {
-    // generate done file
-    if (diffDf.rdd.isEmpty) {
-      return None
-    }
-    val stat = diffDf.groupBy("channelType", "actionType").agg(min("producerEventTs").as("minProducerEventTs"))
-      .select("channelType", "actionType", "minProducerEventTs")
-      .withColumn("minProducerEventTime", datetimeUdf(col("minProducerEventTs")))
-    logger.info("current status {}", Utils.showString(stat, 100, truncate = false))
-
-    val it = doneFileConfig.iterator()
-    var time = Option.empty[ZonedDateTime]
-    while (it.hasNext) {
-      val conf = it.next()
-      time = genDoneFileForEachTeam(diffDf, lastDoneAndDelay, conf)
-    }
-    time
-  }
-
-  def genDoneFileForEachTeam(diffDf: DataFrame, lastDoneAndDelay: mutable.Map[String, (ZonedDateTime, Long)],
-                             config: java.util.Map[String, String]): Option[ZonedDateTime] = {
-
-    val minRowByChannel = diffDf.groupBy("channelType").agg(min("producerEventTs").as("minProducerEventTs"))
-      .select("channelType", "minProducerEventTs").where(config.get("condition"))
-      .orderBy(col("minProducerEventTs"))
-      .withColumn("minProducerEventTime", datetimeUdf(col("minProducerEventTs")))
-
-    if (minRowByChannel.rdd.isEmpty()) {
-      return None;
-    }
-    val minEventTs = minRowByChannel.collectAsList().get(0).getAs[Long]("minProducerEventTs")
-
-    val minProducerEventTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(minEventTs), defaultZoneId)
-
-    val lastDoneThisTime = minProducerEventTime.truncatedTo(ChronoUnit.HOURS).minusHours(1)
-
-    logger.info("Config.name : {}, minProducerEventTs : {}, minProducerEventTime : {}, lastDoneThisTime : {}"
-      , config.get("name"), minEventTs.toString, minProducerEventTime.toString, lastDoneThisTime.toString)
-
-    val times = (1L to ChronoUnit.HOURS.between(lastDoneAndDelay(config.get("name"))._1, lastDoneThisTime))
-      .map(delay => lastDoneAndDelay(config.get("name"))._1.plusHours(delay))
-    if (times.isEmpty) {
-      return None
-    }
-    val doneFilesShouldBeGenerated = times.map(dateTime => getDoneFileNameByConfig(dateTime, doneFileDir, config.get("doneFilePrefix")))
-    logger.info("below done file should be generated {} ", doneFilesShouldBeGenerated)
-    doneFilesShouldBeGenerated.foreach(file => {
-      logger.info("touch hourly done file {}", file)
-      val out = fs.create(new Path(file), true)
-      out.close()
-    })
-    Some(times.last)
-  }
-
   /**
     * Update done files
     * @param inputDateTime input date time. It should be now.
     */
   def updateDoneFiles(inputDateTime: ZonedDateTime): Unit = {
-    val eachLastDone = getEachLastDone(inputDateTime, doneFileDir)
+    val lastDoneAndDelay = getLastDoneFileDateTimeAndDelay(inputDateTime, doneFileDir)
 
     // df cached by last job
     val cacheDf = readCache().withColumnRenamed(eventId, cacheEventId)
 
     // source df after last done timestamp
-    val sourceDf = readSource(eachLastDone("all")._1)
+    val sourceDf = readSource(lastDoneAndDelay._1)
 
     // diff diff, must cache!!
     val diffDf = sourceDf
       .join(cacheDf, col(eventId).===(col(cacheEventId)), "left_anti")
       .cache(this, params.jobDir + "/diffDf")
 
-    val latestDone = genHourlyDoneFile(diffDf, eachLastDone)
+    val latestDone = generateHourlyDoneFile(diffDf, lastDoneAndDelay, inputDateTime)
 
     updateCache(sourceDf, cacheTable, cacheDir, inputDateTime)
 
