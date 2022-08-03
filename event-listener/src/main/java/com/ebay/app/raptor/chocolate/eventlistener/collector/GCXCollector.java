7272c6f8ecfa2000df9899d15e8fd8f6e7d5d15f
yli19 <yli19@ebay.com>
2022-08-03 14:43:04 +0800

    Third party clicks should not be tracked into ubi

--- event-listener/src/main/java/com/ebay/app/raptor/chocolate/eventlistener/collector/GCXCollector.java
+++ event-listener/src/main/java/com/ebay/app/raptor/chocolate/eventlistener/collector/GCXCollector.java
@@ -34,7 +34,7 @@ public class GCXCollector {
         // send click event to ubi
         // Third party clicks should not be tracked into ubi
         if (ChannelActionEnum.CLICK.equals(baseEvent.getActionType()) && (ebaySitesIncludeULK.matcher(baseEvent.getUrl().toLowerCase()).find()
-                || deeplinksites.matcher(baseEvent.getUrl().toLowerCase()).find())) {
+                || deeplinksites.matcher(baseEvent.getUrl().toLowerCase()).find()) && !baseEvent.isThirdParty()) {
             MultiValueMap<String, String> parameters = baseEvent.getUrlParameters();
             IRequestScopeTracker requestTracker = (IRequestScopeTracker) requestContext.getProperty(IRequestScopeTracker.NAME);
             // Ubi tracking
