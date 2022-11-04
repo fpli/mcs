ac95a018a825ff58125c4a09f46d4376ae5b5154
zhijin <zhijin@ebay.com>
2022-11-04 10:33:47 +0800

    feature:Add UTP_DAILY overall done into Hourly Report

--- tools/AlertingAggrate-tool/src/main/java/com/ebay/traffic/chocolate/util/DoneFileUtil.java
+++ tools/AlertingAggrate-tool/src/main/java/com/ebay/traffic/chocolate/util/DoneFileUtil.java
@@ -34,7 +34,6 @@ public class DoneFileUtil {
         // Done file is yesterday,so -1
         c.set(Calendar.DATE, c.get(Calendar.DATE) - 1);
         String currentDate = df.format(c.getTime());
-        String isFileNull=null;
 
         if (list == null || list.size() == 0) {
             int h = LocalDateTime.now().getHour();
@@ -42,14 +41,12 @@ public class DoneFileUtil {
 
             // 0:no delay; 1:delay
             if (pattern.equals("utpbatch_overall")
-                    && hour <= 18) {
+                    && hour <= 17) {
                 delay = 0;
-            }else if (
+            } else if (
                     pattern.equals("utpbatch_overall")
-                            && hour > 18){
-                delay = hour - 18;
-                isFileNull="true";
-
+                            && hour > 17) {
+                delay = hour - 17;
             }
         } else {
             String donefile = "";
@@ -74,7 +71,6 @@ public class DoneFileUtil {
                 // The local 9 a.m. switch to server time is 18 p.m
                 if (!donefile.equals(currentDate) && hour > 18) {
                     delay = hour - 18;
-                    isFileNull="false";
                 } else {
                     delay = 0;
                 }
@@ -85,7 +81,6 @@ public class DoneFileUtil {
 
         retList.add(new Integer(delay).toString());
         retList.add(list.get(0));
-        retList.add(isFileNull);
 
         return retList;
     }
@@ -125,7 +120,6 @@ public class DoneFileUtil {
 
         int delay_hour = Integer.parseInt(list.get(0));
         String donefile = list.get(1);
-        String isFileNull = list.get(2);
 
         DoneFile doneFile = new DoneFile();
         int delay = 0;
@@ -152,7 +146,7 @@ public class DoneFileUtil {
         //01  currentTime is before or after pm 9 o'clock;
         //02  before, statu is OK ; after, judge today_file if is null ?
         //03  not null ,statu is ok; is null ,statu is "Critical"
-        if (pattern.equals("utpbatch_overall") &&  isFileNull.equals("true")) {
+        if (pattern.equals("utpbatch_overall") && delay_hour > 0) {
             status = "Critical";
         }
 
